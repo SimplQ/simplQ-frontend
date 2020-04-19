7aa533c572e585d89b156a62302866d5c68052c7
maaverik <nithinjosetom@gmail.com>
2020-04-19 19:59:08 +0530

    fsdfsd

--- simplq/src/index.js
+++ simplq/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Layout from './Components/Layout';
+import Layout from './components/Layout';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
