f451100afe57a6d6aea872f0b4855322c57885c1
maaverik <nithinjosetom@gmail.com>
2020-04-19 17:13:52 +0530

    Made small fixes

--- simplq/src/index.js
+++ simplq/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './Components/Layout';
+import Layout from './Components/Layout';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Layout />
   </React.StrictMode>,
   document.getElementById('root')
 );
