42141561fae7cf0c171240a1a1474eb0ae238bee
maaverik <nithinjosetom@gmail.com>
2020-04-19 16:54:37 +0530

    Changed home page and moved to Components Folder

---
+++ simplq/src/Components/Layout.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from '../logo.svg';
+import '../App.css';
+import {AppBar} from '@material-ui/core'
+import Button from '@material-ui/core/Button';
+
+function App() {
+  return (
+      <div className="App">
+            <AppBar title="My App">
+                <div>
+                    <h1>
+                        SimplQ
+                    </h1>
+                    <p> Virtual queue generation for everyday use </p>
+                </div>
+                <p> Form here </p>
+                <Button> Create a new Queue </Button>
+            </AppBar>
+        </div>
+  );
+}
+
+export default App;
