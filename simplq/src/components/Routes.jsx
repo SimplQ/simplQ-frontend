a2bcfad0378c8524a86122d1344f2f687c8c1191
daltonfury42 <daltonfury42@users.noreply.github.com>
2020-04-19 19:47:25 +0530

    Admin List Simple (#5)

--- simplq/src/components/Routes.jsx
+++ simplq/src/components/Routes.jsx
@@ -4,14 +4,14 @@ import {
   Route
 } from "react-router-dom";
 import CreateQueue from "./page/CreateQueue"
-import Admin from "./page/Admin"
 import JoinQueue from "./page/JoinQueue";
 import QueueStatus from "./page/QueueStatus";
+import Admin from "./page/admin";
 
 export default () => {
   return <Router>
       <Route path="/" exact component={CreateQueue} />
-      <Route path="/admin" component={Admin} />
+      <Route path="/queue/:queueId/admin" component={Admin} />
       <Route path="/join" component={JoinQueue} />
       <Route path="/queue-status" component={QueueStatus} />
   </Router>
---
+++ simplq/src/components/page/admin/ItemList.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+      maxWidth: 752,
+    },
+    demo: {
+      backgroundColor: theme.palette.background.paper,
+    },
+    title: {
+      margin: theme.spacing(4, 0, 2),
+    },
+  }));
+
+function Item(props) {
+    return <ListItem button>
+        <ListItemAvatar>
+            <Avatar>
+                <AccountCircleIcon />
+            </Avatar>
+        </ListItemAvatar>
+        <ListItemText
+            primary={props.item.name}
+            secondary={props.item.contact}
+        />
+        <ListItemSecondaryAction>
+            <IconButton edge="end" aria-label="delete">
+                <DeleteIcon />
+            </IconButton>
+        </ListItemSecondaryAction>
+    </ListItem>
+}
+
+function ItemList(props) {
+    const classes = useStyles();
+    return <>
+        <Grid item xs={12} md={6}>
+            <Typography variant="h6" className={classes.title}>
+                Queue Users
+          </Typography>
+            <div className={classes.demo}>
+                <List>
+                    {props.items.map(item => <Item item={item} />)}
+                </List>
+            </div>
+        </Grid>
+    </>;
+}
+
+export default ItemList;
