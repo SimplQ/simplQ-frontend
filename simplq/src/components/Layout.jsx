1ab7eadb37acf9f8262906abae58c6640dc29cd8
Nithin Jose Tom <nithinjosetom@gmail.com>
2020-04-19 20:13:31 +0530

    Sq 2 (#6) Made createQueue and made Layout

--- simplq/src/components/Layout.jsx
+++ simplq/src/components/Layout.jsx
@@ -1,26 +1,67 @@
 import React from 'react';
-import logo from '../logo.svg';
-import '../App.css';
-import {AppBar} from '@material-ui/core'
+import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormControl from '@material-ui/core/FormControl';
+import TextField from '@material-ui/core/TextField';
+import Routes from './Routes'
 
-function Layout(props) {
+function Copyright() {
   return (
-      <div className="App">
-            <AppBar title="My App">
-                <div>
-                    <h1>
-                        SimplQ
-                    </h1>
-                    <p> Virtual queue generation for everyday use </p>
-                </div>
-                <Button> Create a new Queue </Button>
-            </AppBar>
-            <div className="main">
-                {props.children}
-            </div>
-        </div>
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
   );
 }
 
-export default Layout;
+const useStyles = makeStyles((theme) => ({
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+}));
+
+export default function Layout() {
+  const classes = useStyles();
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <Typography variant="h6" color="inherit" noWrap>
+            SimplQ
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Routes/>
+
+      {/* Footer */}
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+          By FlatBoys
+        </Typography>
+        <Copyright />
+      </footer>
+      {/* End footer */}
+    </React.Fragment>
+  );
+}
