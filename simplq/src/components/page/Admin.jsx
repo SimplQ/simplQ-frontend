992171f566fc4cb6bfe6973405603c2372eae1a2
Navaneeth Kishore <navaneeth@spotnana.com>
2020-04-19 18:30:34 +0530

    Get queue id

--- simplq/src/components/page/Admin.jsx
+++ simplq/src/components/page/Admin.jsx
@@ -3,6 +3,6 @@ import { withRouter } from 'react-router-dom'
 import React from "react";
 
 function Admin(props) {
-    return <h1>Hello, Admin</h1>;
+return <h1>Hello, Admin {props.match.params.queueId}</h1>;
 }
 export default withRouter(Admin);
