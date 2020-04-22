176fc3dbbe84768dd0c0dc2469e2a0b3be06904e
Navaneeth Kishore <navaneeth@spotnana.com>
2020-04-23 02:43:09 +0530

    Central Section

--- simplq/src/components/page/QueueStatus.jsx
+++ simplq/src/components/page/QueueStatus.jsx
@@ -1,37 +1,25 @@
 import React from "react";
-import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
+import CentralSection from "../CentralSection";
 
-const useStyles = makeStyles((theme) => ({
-    paper: {
-      marginTop: theme.spacing(8),
-      marginBottom: theme.spacing(8),
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-    }
-}));
 
 function QueueStatus(props) {
-    var aheadCount = 10;
-    const classes = useStyles()
+  var aheadCount = 10;
 
-    return <Container maxWidth="sm" component="main">
-        <div className={classes.paper}>
-    <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-      Title?
-    </Typography>
+  return <CentralSection heading="Your Status">
     <Typography variant="h5" align="center" color="textSecondary" component="p">
-    There are {aheadCount} people ahead of you. Thanks for waiting!
+      There are {aheadCount} people ahead of you. Thanks for waiting!
     </Typography>
-    <br />
-    <Button variant="contained" color="primary" >
-        Refresh
-      </Button>
-</div>
-  </Container>
+    <div style={{ display: "flex", justifyContent: 'flex-end' }}>
+            <Button variant="contained" color="primary" style={{
+                marginTop: 30,
+                marginLeft: 10,
+            }}>
+                Refresh
+        </Button>
+        </div>
+  </CentralSection>
 }
 
 export default QueueStatus
