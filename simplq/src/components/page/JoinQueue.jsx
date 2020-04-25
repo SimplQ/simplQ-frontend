ee0089e0b2c93ff76b4177616d17e32f96356ead
sajmalyousef <yousefsajmal1@gmail.com>
2020-04-25 15:24:33 +0530

    Added contact input field

--- simplq/src/components/page/JoinQueue.jsx
+++ simplq/src/components/page/JoinQueue.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import CentralSection from "../CentralSection";
+import PhoneInput from 'react-phone-input-2';
+import 'react-phone-input-2/lib/material.css';
+
+function Example() {
+    const [value, setValue] = useState()
+    return (
+        <PhoneInput
+            placeholder="Contact Number"
+            country={'in'}
+            value={value}
+            inputProps={{
+                name: 'phone',
+                required: true,
+                autoFocus: true
+
+            }}
+            inputStyle={{
+                width: '100%'
+            }}
+            onChange={setValue} />
+    )
+}
 
 function JoinQueue(props) {
     return <CentralSection heading="Join Queue">
@@ -14,17 +36,10 @@ function JoinQueue(props) {
                 shrink: true,
             }}
             variant="outlined"
+            inputStyle="material"
         />
-        <TextField
-            placeholder="Contact Number"
-            fullWidth
-            required
-            margin="normal"
-            InputLabelProps={{
-                shrink: true,
-            }}
-            variant="outlined"
-        />
+        <br />
+        {Example()}
         <div style={{ display: "flex", justifyContent: 'flex-end' }}>
             <Button variant="contained" color="primary" style={{
                 marginTop: 30,
