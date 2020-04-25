cacabe081dc27b3faa84f0f9ff9fdd15b21f47b2
Navaneeth Kishore <navaneeth@spotnana.com>
2020-04-25 22:35:52 +0530

    Tidy up Admin Page

--- simplq/src/components/page/admin/index.jsx
+++ simplq/src/components/page/admin/index.jsx
@@ -1,64 +1,70 @@
-import React from "react";
+import React, { useState } from 'react';
 import ItemList from "./ItemList";
 import { CopyToClipboard } from 'react-copy-to-clipboard';
-import AddIcon from '@material-ui/icons/Add';
-import RefreshIcon from '@material-ui/icons/Refresh';
-import { IconButton, Button } from '@material-ui/core';
-import FileCopyIcon from '@material-ui/icons/FileCopy';
-import { getQueuedItems } from '../../../apis'
+import { Button } from '@material-ui/core';
 import CentralSection from "../../CentralSection";
+import Alert from '@material-ui/lab/Alert';
+import { makeStyles } from '@material-ui/core/styles';
 
-export default class Admin extends React.Component {
-    state = {
-        items: null,
-        queueId: this.props.match.params.queueId
+const useStyles = makeStyles((theme) => ({
+    button: {
+        marginLeft: theme.spacing(1)
+    },
+    urlBox: {
+        margin: theme.spacing(3)
     }
+}));
 
-    componentDidMount() {
-        this.loadItems(this.state.queueId)
+function content(items) {
+    if (items == null) {
+        return <p>Loading...</p>
     }
-
-    loadItems(queueId) {
-        const items = getQueuedItems(queueId);
-        this.setState({ items });
-    }
-
-    content() {
-        if (this.state.items == null) {
-            return <p>Loading...</p>
-        }
-        if (this.state.items.length > 0) {
-            return <ItemList items={this.state.items} />
-        } else {
-            return <p>Empty Queue</p>
-        }
-    }
-
-    render() {
-        var shareUrl = window.location.origin + "/" + this.state.queueId;
-        return <CentralSection heading="Queue Dashboard">
-
-            <p> Share this url with others to get started:</p>
-            <p>{shareUrl} <CopyToClipboard text={shareUrl}>
-                <IconButton><FileCopyIcon /></IconButton>
-            </CopyToClipboard> </p>
-
-            {this.content()}
-
-            <IconButton aria-label="share">
-                <AddIcon />
-            </IconButton>
-            <IconButton aria-label="share">
-                <RefreshIcon />
-            </IconButton>
-            <div style={{ display: "flex", justifyContent: 'flex-end' }}>
-                <Button variant="contained" color="primary" style={{
-                    marginTop: 30,
-                    marginLeft: 10,
-                }}>
-                    Refresh
-                </Button>
-            </div>
-        </CentralSection>
+    if (items.length > 0) {
+        return <ItemList items={items} />
+    } else {
+        return <p>Empty Queue</p>
     }
 }
+
+
+export default (props) => {
+    const [items, setItems] = useState([
+        {
+            "name": "Nithin Jose",
+            "contact": "9400413350",
+            "id": 1
+        },
+        {
+            "name": "Navaneeth Kishore",
+            "contact": "9446011567",
+            "id": 2
+        }
+    ]);
+
+    const classes = useStyles();
+    var shareUrl = window.location.origin + "/" + props.match.params.queueId;
+
+    return <CentralSection heading="Shobha Supermart">
+        <Alert severity="info" className={classes.urlBox}
+            action={
+                <CopyToClipboard text={shareUrl}>
+                    <Button color="inherit" size="small">
+                        COPY
+                            </Button>
+                </CopyToClipboard>
+            }
+        >
+            {shareUrl}
+        </Alert>
+        {content(items)}
+        <div style={{ display: "flex", justifyContent: 'flex-end' }}>
+            <Button variant="contained" color="primary" className={classes.botton}>
+                Add
+            </Button>
+            <Button variant="contained" color="primary" className={classes.button}>
+                Refresh
+            </Button>
+        </div>
+    </CentralSection>
+}
+
