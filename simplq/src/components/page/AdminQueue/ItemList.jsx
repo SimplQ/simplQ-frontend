445447797195efa29ed374fb4b9e1ae0bc78c3b7
Navaneeth Kishore <navaneeth@spotnana.com>
2020-04-26 23:22:13 +0530

    admin layout

--- simplq/src/components/page/AdminQueue/ItemList.jsx
+++ simplq/src/components/page/AdminQueue/ItemList.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
@@ -7,18 +6,21 @@ import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
-import { Avatar, Card } from "@material-ui/core";
+import { Avatar, Card, Typography } from "@material-ui/core";
+import CallIcon from '@material-ui/icons/Call';
 
 function Item(props) {
-    return <ListItem button>
+    const contact = props.item.contact;
+    const name = props.item.name;
+    return <ListItem button component="a" href={"tel:" + contact}>
         <ListItemAvatar>
             <Avatar>
-                <AccountCircleIcon />
+                <CallIcon />
             </Avatar>
         </ListItemAvatar>
         <ListItemText
-            primary={props.item.name}
-            secondary={props.item.contact}
+            primary={name}
+            secondary={contact}
         />
         <ListItemSecondaryAction>
             <IconButton edge="end" aria-label="delete">
@@ -29,11 +31,18 @@ function Item(props) {
 }
 
 function ItemList(props) {
+     
     return (
         <Card >
-            <List>
-                {props.items.map(item => <Item item={item} key={item.id} />)}
-            </List>
+            { !props.items || props.items.length === 0 ? 
+                <Typography variant="subtitle1" align="center" style={{margin: "20px 20px"}} gutterBottom>
+                    Waiting for users to jain the queue
+                </Typography>
+            :
+                <List>
+                    {props.items.map(item => <Item item={item} key={item.id} />)}
+                </List>
+            }
         </Card>);
 }
 
