6a57152478618acd71fe1e5a9fabbc23200494a4
Navaneeth Kishore <navaneeth@spotnana.com>
2020-05-09 19:43:40 +0530

    padding

--- simplq/src/components/CentralSection.js
+++ simplq/src/components/CentralSection.js
@@ -6,11 +6,9 @@ import Skeleton from '@material-ui/lab/Skeleton';
 
 const useStyles = makeStyles((theme) => ({
     paper: {
-        marginTop: theme.spacing(3),
         marginBottom: theme.spacing(3),
         padding: theme.spacing(2),
         [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
-            marginTop: theme.spacing(6),
             marginBottom: theme.spacing(6),
             padding: theme.spacing(3),
         },
