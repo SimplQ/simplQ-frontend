04377bac748e6a91918366d4783dc5503529cd50
Navaneeth Kishore <navaneeth@spotnana.com>
2020-05-10 21:22:30 +0530

    404

--- simplq/src/components/Routes.jsx
+++ simplq/src/components/Routes.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import {
-  Route
+  Route, Switch
 } from "react-router-dom";
 import CreateQueue from "./page/CreateQueue"
 import JoinQueue from "./page/JoinQueue";
 import QueueStatus from "./page/QueueStatus";
 import AdminQueue from "./page/AdminQueue";
+import PageNotFound from "./page/PageNotFound";
 
 export default () => {
-  return <>
+  return <Switch>
       <Route path="/" exact component={CreateQueue} />
       <Route path="/admin" exact component={AdminQueue} />
       <Route path="/j/:queueId" exact component={JoinQueue} />
       <Route path="/status" exact component={QueueStatus} />
-  </>
+      <Route component={PageNotFound} />
+  </Switch>
 }
--- simplq/src/components/page/AdminQueue/index.jsx
+++ simplq/src/components/page/AdminQueue/index.jsx
@@ -6,6 +6,7 @@ import QueueService from '../../../services/queue';
 import { useSelector, useDispatch } from 'react-redux';
 import { setQueueName, progressStep } from '../../../store/appSlice';
 import ShareBar from './ShareBar';
+import PageNotFound from '../PageNotFound';
 
 const useStyles = makeStyles((theme) => ({
     urlBox: {
@@ -26,7 +27,8 @@ export default () => {
     const queueName = useSelector((state) => state.appReducer.queueName);
 
     if(!queueId) {
-        throw new Error('Queue Not Created. TODO 404')
+        // If queue id is not here, most probably his session storage got cleared. This can be solved only with proper auth.
+        return <PageNotFound />
     }
 
     dispatch(progressStep(1))
