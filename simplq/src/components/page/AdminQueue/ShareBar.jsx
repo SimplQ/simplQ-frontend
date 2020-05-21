20fa78b456b6f7c68c2484737993948004ac9aa8
maaverik <nithinjosetom@gmail.com>
2020-05-22 00:41:58 +0530

    Changed link display and hid number

--- simplq/src/components/page/AdminQueue/ShareBar.jsx
+++ simplq/src/components/page/AdminQueue/ShareBar.jsx
@@ -1,32 +1,71 @@
 import React from 'react';
 import Skeleton from '@material-ui/lab/Skeleton';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
-import Alert from '@material-ui/lab/Alert';
+//import Alert from '@material-ui/lab/Alert';
 import { Button } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 import { progressCreationStep } from '../../../store/appSlice';
+import { Typography } from '@material-ui/core';
+import Tooltip from '@material-ui/core/Tooltip';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
 
 const ShareBar = (props) => {
     const queueId = props.queueId;
     const dispatch = useDispatch();
+
+    const [open, setOpen] = React.useState(false);
+
+    const handleTooltipClose = () => {
+        setOpen(false);
+    };
+
+    const handleTooltipOpen = () => {
+        setOpen(true);
+    };
 
     if (!queueId) {
         return <Skeleton variant="rect" height={68} />
     }
 
     var shareUrl = window.location.origin + "/j/" + queueId;
+
     return (<>
-        <Alert severity="info" className={props.className}
-            action={
-                <CopyToClipboard text={shareUrl}>
-                    <Button color="inherit" size="small" onClick={() => dispatch(progressCreationStep(2))}>
-                        COPY
-                    </Button>
-                </CopyToClipboard>
-            }
+        <Typography 
+            align='center'
+            color='initial'
+            display='block'
+            gutterBottom={true}
         >
-            https://simplq.me/j/todo-ellipsis
-        </Alert>
+
+            <ClickAwayListener onClickAway={handleTooltipClose}>
+                <div>
+                    <Tooltip
+                        PopperProps={{disablePortal: true,}}
+                        onClose={handleTooltipClose}
+                        open={open}
+                        disableFocusListener
+                        disableHoverListener
+                        disableTouchListener
+                        title="Copied to clipboard"
+                    >
+                        <CopyToClipboard text={shareUrl}>
+                            <Button color="inherit" size="small" 
+                                style={{ 'backgroundColor': '#add8e6', 'overflow': 'auto' }}
+                                onClick={
+                                    () => {
+                                        dispatch(progressCreationStep(2));
+                                        handleTooltipOpen();
+                                    }
+                                }
+                            >
+                                {'Click here to copy your queue link'}
+                            </Button>
+                        </CopyToClipboard>
+                    </Tooltip>
+                </div>
+            </ClickAwayListener>
+
+        </Typography>
         </>
     );
 }
