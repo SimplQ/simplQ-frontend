fc39b210d8eb739ecb65a52911c0d2703b176b55
daltonfury42 <daltonfury42@users.noreply.github.com>
2020-06-07 18:22:51 +0530

    GitHub Fork ribbon (#92)

--- simplq/src/index.js
+++ simplq/src/index.js
@@ -3,16 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Layout from './components/Layout';
 import * as serviceWorker from './serviceWorker';
+import GitHubForkRibbon from 'react-github-fork-ribbon';
+
 import {
   BrowserRouter as Router
 } from "react-router-dom";
 import { Provider } from 'react-redux'
 import { store } from './store'
 
+const Content = () => (
+  <GitHubForkRibbon href="//github.com/SimplQ/simplQ-frontend"
+    target="_blank"
+    position="right">
+    Fork me on GitHub
+  </GitHubForkRibbon>
+);
+
 ReactDOM.render(
-    <Router> 
-      <Provider store={store}> <Layout /> </Provider> </Router>
-,
+  <>
+    <Router>
+      <Provider store={store}> <Layout /> </Provider>
+    </Router>
+    <Content />
+  </>
+  ,
   document.getElementById('root')
 );
 
