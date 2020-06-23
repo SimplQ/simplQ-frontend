0057f93183bec285a0368c9bbbd042df0a306120
maaverik <nithinjosetom@gmail.com>
2020-06-24 00:50:00 +0530

    Normal buttons

--- simplq/src/components/design/Button.jsx
+++ simplq/src/components/design/Button.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
 import '../../styles/buttons.scss';
-import { StylesProvider } from '@material-ui/core/styles';
 
 const StandardButton = (props) => {
   const { onClick, text } = props;
   return (
-    <StylesProvider injectFirst>
-      <Button onClick={onClick} className="standard-button">
-        {text}
-      </Button>
-    </StylesProvider>
+    <button type="button" onClick={onClick} className="standard-button">
+      {text}
+    </button>
   );
 };
 
