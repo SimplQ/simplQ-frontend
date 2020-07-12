f967a0c00f2ab0fcdc57c7e846557285c0ba8cf8
thehamzarocks <thehamzarocks@gmail.com>
2020-07-12 22:51:19 +0530

    Using the useCallback hook for memoizing functions for useEffect (#132)
    polling for status changes in admin and waiting pages

--- simplq/src/components/pages/QueueStatus.jsx
+++ simplq/src/components/pages/QueueStatus.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Typography from '@material-ui/core/Typography';
 import { CircularProgress } from '@material-ui/core';
@@ -10,6 +10,7 @@ import { handleApiErrors } from '../ErrorHandler';
 import styles from '../../styles/statusPage.module.scss';
 import Button from '../common/Button';
 import Header, { SimplQHeader } from '../common/Header';
+import NotificationButton from '../common/NotificationButton';
 
 const TIMEOUT = 10000;
 let timeoutId;
@@ -22,13 +23,32 @@ function QueueStatus() {
   const aheadCount = useSelector((state) => state.appReducer.aheadCount);
   const [updateInProgress, setUpdateInProgress] = useState(false);
 
-  const update = () => {
+  const showNotification = useCallback(() => {
+    const notificationImage = '/LogoLight.png';
+    const notificationText = `${queueName}: You've been notified by the queue manager.`;
+    const notification = new Notification('SimplQ', {
+      body: notificationText,
+      icon: notificationImage,
+    });
+    document.addEventListener('visibilitychange', () => {
+      if (document.visibilityState === 'visible') {
+        // The tab has become visible so clear the now-stale Notification.
+        notification.close();
+      }
+    });
+  }, [queueName]);
+
+  const update = useCallback(() => {
     clearTimeout(timeoutId);
     if (tokenId) {
+      const oldTokenStatus = tokenStatus;
       TokenService.get(tokenId)
         .then((response) => {
           dispatch(setAheadCount(response.aheadCount));
           setTokenStatus(response.tokenStatus);
+          if (response.tokenStatus === 'NOTIFIED' && oldTokenStatus === 'WAITING') {
+            showNotification();
+          }
           timeoutId = setTimeout(update, TIMEOUT);
         })
         .catch((err) => {
@@ -36,12 +56,12 @@ function QueueStatus() {
           timeoutId = setTimeout(update, TIMEOUT);
         });
     }
-  };
+  }, [tokenId, tokenStatus, dispatch, showNotification]);
 
   useEffect(() => {
     update();
     return () => clearTimeout(timeoutId);
-  }, [tokenId]);
+  }, [update]);
 
   const onDeleteClick = () => {
     setUpdateInProgress(true);
@@ -85,6 +105,7 @@ function QueueStatus() {
       <Header text={queueName} className={styles.header} />
       <JoinerStepper />
       <div>{status}</div>
+      <NotificationButton />
       {!(tokenStatus === 'REMOVED') && !updateInProgress ? (
         <div className={styles['button-group']}>
           <div>
