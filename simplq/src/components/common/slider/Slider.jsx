b28eaffea53ff7ce57dac270bc33742276c50cad
daltonfury42 <daltonfury42@disroot.org>
2020-07-25 19:08:11 +0530

    fix css

--- simplq/src/components/common/slider/Slider.jsx
+++ simplq/src/components/common/slider/Slider.jsx
@@ -6,7 +6,7 @@ import styles from '../../../styles/slider.module.scss';
 const Slide = (props) => {
   console.log('This is something tiny missing somehwere...');
   return (
-    <div>
+    <div className={styles['slider-content']}>
       <img src={props.slide.imgSrc} alt={props.slide.imgAlt} />
       <h4>{props.slide.title}</h4>
       <p>{props.slide.subtitle}</p>
