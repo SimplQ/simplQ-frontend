c638c4ac9c13355cbc14866d7607e4919bb438a5
daltonfury42 <daltonfury42@disroot.org>
2020-07-25 19:38:56 +0530

    remove console.log

--- simplq/src/components/common/slider/Slider.jsx
+++ simplq/src/components/common/slider/Slider.jsx
@@ -4,7 +4,6 @@ import 'react-awesome-slider/dist/styles.css';
 import styles from '../../../styles/slider.module.scss';
 
 const Slide = (props) => {
-  console.log('This is something tiny missing somehwere...');
   return (
     <div className={styles['slider-content']}>
       <img src={props.slide.imgSrc} alt={props.slide.imgAlt} />
