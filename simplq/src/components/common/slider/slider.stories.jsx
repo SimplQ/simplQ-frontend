dbb2fbdecf9aa51ea069f2d170edff99d33c1595
daltonfury42 <daltonfury42@disroot.org>
2020-07-25 13:20:15 +0530

    add content

--- simplq/src/components/common/slider/slider.stories.jsx
+++ simplq/src/components/common/slider/slider.stories.jsx
@@ -14,16 +14,16 @@ const creatorSlides = [
     subtitle: 'Enter queue name and create a queue totally free of cost',
   },
   {
-    imgSrc: 'http://localhost:3000/images/check_status.svg',
-    imgAlt: 'create',
-    title: 'Should Fill',
-    subtitle: 'Fill the description',
+    imgSrc: 'http://localhost:3000/images/share.svg',
+    imgAlt: 'share',
+    title: 'Share Queue',
+    subtitle: 'Share the queue to your users for them to join',
   },
   {
-    imgSrc: 'http://localhost:3000/images/minimize_crowding.svg',
-    imgAlt: 'create',
-    title: 'Should Fill',
-    subtitle: 'Fill the description',
+    imgSrc: 'http://localhost:3000/images/manage.svg',
+    imgAlt: 'manage',
+    title: 'Manage Queue',
+    subtitle: 'Manage your queue seamlessly and efficiently',
   },
 ];
 
@@ -33,22 +33,16 @@ export const CreatorSlider = () => {
 
 const joinerSlides = [
   {
-    imgSrc: 'http://localhost:3000/images/queue_creation.svg',
-    imgAlt: 'create',
-    title: 'Create Queue',
-    subtitle: 'Enter queue name and create a queue totally free of cost',
+    imgSrc: 'http://localhost:3000/images/join.svg',
+    imgAlt: 'join',
+    title: 'Join Queue',
+    subtitle: 'Get the queue link and join from anywhere',
   },
   {
-    imgSrc: 'http://localhost:3000/images/check_status.svg',
-    imgAlt: 'create',
-    title: 'Should Fill',
-    subtitle: 'Fill the description',
-  },
-  {
-    imgSrc: 'http://localhost:3000/images/minimize_crowding.svg',
-    imgAlt: 'create',
-    title: 'Should Fill',
-    subtitle: 'Fill the description',
+    imgSrc: 'http://localhost:3000/images/finish.svg',
+    imgAlt: 'wait',
+    title: 'Wait for your turn',
+    subtitle: 'Get notified when your turn is up without physically being there',
   },
 ];
 
