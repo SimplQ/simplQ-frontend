328927151e83495cc82b8dba3999db7c0c679622
jithinlal <jithtitan007@gmail.com>
2020-08-18 22:26:30 +0530

    button design change

--- simplq/src/components/common/Button/Button.jsx
+++ simplq/src/components/common/Button/Button.jsx
@@ -4,11 +4,7 @@ import styles from '../../../styles/buttons.module.scss';
 const StandardButton = (props) => {
   const { onClick } = props;
   return (
-    <button
-      type="submit"
-      onClick={onClick}
-      className={props.outlined ? styles['standard-button-outlined'] : styles['standard-button']}
-    >
+    <button type="submit" onClick={onClick} className={styles['standard-button']}>
       {props.children}
     </button>
   );
