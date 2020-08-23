341be4dfbe8239448564f1eb35e8e3b32cfd2ae6
jithinlal <jithtitan007@gmail.com>
2020-08-23 22:00:09 +0530

    center collapsible

--- simplq/src/components/common/Collapsible/Collapsible.jsx
+++ simplq/src/components/common/Collapsible/Collapsible.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'react-awesome-slider/dist/styles.css';
-import { Collapse, ListItemIcon } from '@material-ui/core';
+// import styles from '../../../styles/collapsible.module.scss';
+import { Collapse, ListItemIcon, Grid } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 import { ExpandLess, ExpandMore } from '@material-ui/icons';
 import ListItem from '@material-ui/core/ListItem';
@@ -10,18 +11,21 @@ const StandardCollapsible = (props) => {
   const [open, setOpen] = React.useState(false);
 
   return (
-    <>
-      <ListItem button onClick={() => setOpen(!open)}>
-        <ListItemIcon>
-          <AddIcon />
-        </ListItemIcon>
-        <ListItemText primary={props.text} />
-        {open ? <ExpandLess /> : <ExpandMore />}
-      </ListItem>
-      <Collapse in={open} timeout="auto" unmountOnExit>
-        {props.children}
-      </Collapse>
-    </>
+    <Grid container direction="column" alignItems="center" justify="center">
+      <Grid item cs={6}>
+        <ListItem button onClick={() => setOpen(!open)}>
+          <ListItemIcon>
+            <AddIcon />
+          </ListItemIcon>
+          <ListItemText primary={props.text} />
+          {open ? <ExpandLess /> : <ExpandMore />}
+        </ListItem>
+
+        <Collapse in={open} timeout="auto" unmountOnExit>
+          {props.children}
+        </Collapse>
+      </Grid>
+    </Grid>
   );
 };
 
