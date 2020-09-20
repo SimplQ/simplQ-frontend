c171d6a061ccec306a8f42c8bbb5bdc568e46494
daltonfury42 <daltonfury42@users.noreply.github.com>
2020-09-20 20:51:51 +0530

    Issues/212 - preview (#243)
    * add footer
    
    * refactor
    
    Co-authored-by: Mehmet Firat KOMURCU <mehmetfiratkomurcu@hotmail.com>
    Co-authored-by: Mehmet Fırat Kömürcü <firat.komurcu@trendyol.com>

--- simplq/src/components/pages/Home/StaticInfos.jsx
+++ simplq/src/components/pages/Home/StaticInfos.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from '../../../styles/homePage.module.scss';
 import { CreatorSlider, JoinerSlider } from '../../common/slider';
+import { ForkOnGithubButton } from '../../common/Button/Button.stories';
 
 export const BenefitsInfo = () => (
   <div className={styles['benefits-info']}>
@@ -52,6 +53,87 @@ export const ExtraInfo = () => (
     <div>
       <img src="/images/easy.svg" alt="easy to use" />
       <p>Easy to use</p>
+    </div>
+  </div>
+);
+
+export const Footer = () => (
+  <div className={styles.footer}>
+    <div className={styles['main-card-container']}>
+      <div className={styles.card}>
+        <div className={styles['card-header']}>
+          <div className={styles.simpleQ_image_container}>
+            <img
+              src="/images/new_logo.svg"
+              className={styles['new-logo-image']}
+              alt="simplQ new logo"
+            />
+          </div>
+          <div className={styles.logo_header_container}>
+            <h1>SimplQ</h1>
+          </div>
+        </div>
+        <div className={styles['card-body']}>
+          <div className={styles['description-content-container']}>
+            <b className={styles['description-content']}>
+              SimplQ is a completely web based queue management solution that anyone can use to
+              create instant virtual queues.
+            </b>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.card}>
+        <div className={styles['card-header']}>
+          <div>
+            <h1>Open source</h1>
+          </div>
+        </div>
+        <div className={styles['card-body']}>
+          <div className={styles['open-source-content-container']}>
+            <p>
+              <b>
+                <span>SimplQ </span>
+                is open source. Be part of the
+                <span> SimplQ </span>
+                community.
+              </b>
+            </p>
+            <ForkOnGithubButton />
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.card}>
+        <div className={styles['card-header']}>
+          <div>
+            <h1>Keep in touch</h1>
+          </div>
+        </div>
+        <div className={styles['card-body']}>
+          <div className={styles['list-container']}>
+            <ul>
+              <li>
+                <a href="https://github.com/SimplQ/simplQ-frontend">Github</a>
+              </li>
+              <li>
+                <a href="https://medium.com/@raimazach/virtualizing-queues-a-long-overdue-alternative-to-physical-queues-bfdc4b51070f">
+                  Medium
+                </a>
+              </li>
+              <li>
+                <a href="https://www.youtube.com/channel/UCAb9PSXvrGZ4vvSneK1Nrow">Youtube</a>
+              </li>
+              <li>
+                <a href="https://www.facebook.com/simplq/">Facebook</a>
+              </li>
+              <li>
+                <a href="mailto:contact@simplq.me">Gmail</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 );
