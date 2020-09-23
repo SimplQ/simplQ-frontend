c5d52a3142d5ed55e651a4d0b0db89e5fe2d24c4
Lint Action <lint-action@samuelmeuli.com>
2020-09-23 15:41:59 +0000

    Fix code style issues with Prettier

--- simplq/src/components/pages/Home/Nav/Navbar.jsx
+++ simplq/src/components/pages/Home/Nav/Navbar.jsx
@@ -3,13 +3,13 @@ import styles from '../../../../styles/homePage.module.scss';
 import Burger from './Burger';
 
 const Navbar = () => {
-    return (
-        <nav className={styles['Navbar-style']}>
-            <img src="LogoLight.png" className="logo" height="50" width="50" />
+  return (
+    <nav className={styles['Navbar-style']}>
+      <img src="LogoLight.png" className="logo" height="50" width="50" />
 
-            <Burger />
-        </nav>
-    )
-}
+      <Burger />
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- simplq/src/components/pages/Home/Nav/RightNav.jsx
+++ simplq/src/components/pages/Home/Nav/RightNav.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import styles from '../../../../styles/homePage.module.scss';
 
-
 const RightNav = ({ open }) => {
   return (
-    <ul className={styles['Menu-List-style']} open={open} >
-      <li><a href="#" className="nostyle">Home</a></li>
-      <li><a href="#" className="nostyle">Contact Us</a></li>
+    <ul className={styles['Menu-List-style']} open={open}>
+      <li>
+        <a href="#" className="nostyle">
+          Home
+        </a>
+      </li>
+      <li>
+        <a href="#" className="nostyle">
+          Contact Us
+        </a>
+      </li>
     </ul>
-  )
-}
+  );
+};
 
-export default RightNav
+export default RightNav;
