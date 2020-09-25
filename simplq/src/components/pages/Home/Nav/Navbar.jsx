bca19dcfd600e5e90d5ebe594a194d925618379b
Archana Kumari <arkumari2000@gmail.com>
2020-09-25 20:33:07 +0530

    Non-responsive navbar for home page (#253)

---
+++ simplq/src/components/pages/Home/Nav/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from '../../../../styles/homePage.module.scss';
+import Burger from './Burger';
+
+const Navbar = () => {
+    return (
+        <nav className={styles['navbar']}>
+            <img src="LogoLight.png" />
+
+            <Burger />
+        </nav>
+    )
+}
+
+export default Navbar
