640714dfb4949c217d4842d8d9d0a0f6744a696b
Lint Action <lint-action@samuelmeuli.com>
2020-09-26 17:10:56 +0000

    Fix code style issues with Prettier

--- simplq/src/components/pages/Home/Nav/Burger.jsx
+++ simplq/src/components/pages/Home/Nav/Burger.jsx
@@ -3,17 +3,17 @@ import styles from '../../../../styles/homePage.module.scss';
 import RightNav from './RightNav';
 
 const Burger = () => {
-    const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
 
-    return (
-        <>
-            <div className={styles['burger']} open={open} onClick={() => setOpen(!open)}>
-                <div />
-                <div />
-                <div />
-            </div>
-            <RightNav open={open} />
-        </>
-    )
-}
-export default Burger
+  return (
+    <>
+      <div className={styles['burger']} open={open} onClick={() => setOpen(!open)}>
+        <div />
+        <div />
+        <div />
+      </div>
+      <RightNav open={open} />
+    </>
+  );
+};
+export default Burger;
