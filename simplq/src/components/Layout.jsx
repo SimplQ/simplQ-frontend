52c9374c9405ec0814634abea64c5df080f08fa4
Anubhav Choudhary <57551996+deprov447@users.noreply.github.com>
2020-10-02 19:37:27 +0530

    [UI Change] Footer included in all routes (#286)

--- simplq/src/components/Layout.jsx
+++ simplq/src/components/Layout.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Routes from './Routes';
+import { Footer } from './common/Footer'
 
 function Layout() {
   return (
     <>
       <Routes />
+      <Footer />
     </>
   );
 }
