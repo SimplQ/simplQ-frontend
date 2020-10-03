80cbfb84d43d56d234dd66b16b9ab5219b35ad2b
gh05d <pc@vipfy.com>
2020-10-03 14:19:49 +0200

    Fixed errors and warnings

--- simplq/src/components/pages/Status/ContactAdmin.jsx
+++ simplq/src/components/pages/Status/ContactAdmin.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import ChatIcon from '@material-ui/icons/Chat';
+import IconButton from '@material-ui/core/IconButton';
 import styles from '../../../styles/statusPage.module.scss';
 
 export default () => {
   return (
-    <>
-      <div className={styles['user-action']}>
-        {/* icon */}
-        <div>
-          <h2>Contact Admin</h2>
-          <p>Contact the queue admin for queries</p>
-        </div>
+    <li className={styles['user-action']}>
+      <IconButton aria-label="Chat">
+        <ChatIcon />
+      </IconButton>
+
+      <div>
+        <h2>Contact Admin</h2>
+        <p>Contact the queue admin for queries</p>
       </div>
-    </>
+    </li>
   );
 };
