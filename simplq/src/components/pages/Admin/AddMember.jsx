f985bab233930f36ee0baef648bd8285acf27ef3
mritunjaysaha <59063979+mritunjaysaha@users.noreply.github.com>
2020-10-03 20:46:33 +0530

    added expand more icon

--- simplq/src/components/pages/Admin/AddMember.jsx
+++ simplq/src/components/pages/Admin/AddMember.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import AddIcon from '@material-ui/icons/Add';
 import styles from '../../../styles/adminPage.module.scss';
 
+const stylesExpand = {
+  height: '10vh',
+  width: '10vw',
+  background: '#f00',
+};
+
 export default () => {
   return (
     <>
@@ -11,7 +17,12 @@ export default () => {
           <h2>Add Member Manually</h2>
           <p>Add a person to this queue manually</p>
         </div>
+        <div>
+          <img src="/images/expand_more.svg" alt="expand" />
+        </div>
       </div>
+
+      <div style={stylesExpand} />
     </>
   );
 };
