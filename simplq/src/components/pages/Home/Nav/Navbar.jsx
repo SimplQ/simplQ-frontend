7d3141cb6a555a770b82fa94ef67420f4c586cc4
Nithin Jose Tom <nithinjosetom@gmail.com>
2020-10-04 22:37:14 +0530

    Scroll to top on logo click on home page (#341)
    * Hompe page bottom redesign using table
    
    * Made footer smaller
    
    * A little refactoring and scroll to top on logo click on home page
    
    * Fix padding blocking a part of logo

--- simplq/src/components/pages/Home/Nav/Navbar.jsx
+++ simplq/src/components/pages/Home/Nav/Navbar.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import styles from '../../../../styles/homePage.module.scss';
 import Burger from './Burger';
 import Logo from '../../../common/ClickableLogo';
+import { smoothScrollTo } from '../../../common/utilFns';
 
 const Navbar = () => {
+  const scrollToTop = () => {
+    const element = document.getElementById('target_top');
+    smoothScrollTo(element);
+  };
+
   return (
     <nav className={styles['navbar']}>
-      <Logo />
+      <Logo onClick={scrollToTop} />
       <Burger />
     </nav>
   );
