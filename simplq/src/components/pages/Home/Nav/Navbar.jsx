c32c8db8bd95edb49b9f1b49b78c76ebbb19ac8c
Mritunjay Saha <mritunjaysaha17@gmail.com>
2020-10-04 14:42:38 +0530

    SimplQ logo and name do not match  (#324)
    * added verified icon
    
    * moved copy to clipboard expression to a function
    
    * moved verified to public/images
    
    * completed new tokens
    
    * removed commented lines
    
    * removed unused classname
    
    * changed width to 40rem
    
    * added expand more icon
    
    * added JoinQueueForm
    
    * aligned join queue form
    
    * completed Add Member Manually
    
    * completed Add Member Manually
    
    * made changes
    
    * added max-width: 56vh to side-panel
    
    * changed 56vh to 35rem
    
    * added cursor pointer to logo and burger

--- simplq/src/components/pages/Home/Nav/Navbar.jsx
+++ simplq/src/components/pages/Home/Nav/Navbar.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import styles from '../../../../styles/homePage.module.scss';
 import Burger from './Burger';
 
+const Logo = () => (
+  <div className={styles['logo']}>
+    <img src="LogoLight.png" alt="Home" />
+    <p>SimplQ</p>
+  </div>
+);
+
 const Navbar = () => {
   return (
     <nav className={styles['navbar']}>
-      <img src="LogoLight.png" alt="Home" />
-      <p>SimplQ</p>
+      <Logo />
       <Burger />
     </nav>
   );
