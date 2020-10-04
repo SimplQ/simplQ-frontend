5ba599d4fd8038fd0b66b3655a45640556dc84e9
daltonfury42 <daltonfury42@users.noreply.github.com>
2020-10-04 22:33:56 +0530

    Add Contact Us form to footer (#340)

--- simplq/src/components/common/Footer.jsx
+++ simplq/src/components/common/Footer.jsx
@@ -59,6 +59,11 @@ export default () => (
           <div className={styles['list-container']}>
             <ul>
               <li>
+                <a target="_blank" href="https://iimb.qualtrics.com/jfe/form/SV_aY8FY91ztRX9NvD">
+                  Contact Us
+                </a>
+              </li>
+              <li>
                 <a href="https://github.com/SimplQ/simplQ-frontend">Github</a>
               </li>
               <li>
