e594dc5d1ac8c548d635052be8e0a504e9e1a224
Lint Action <lint-action@samuelmeuli.com>
2020-10-04 17:15:20 +0000

    Fix code style issues with Prettier

--- simplq/src/components/pages/Home/Nav/LeftNav.jsx
+++ simplq/src/components/pages/Home/Nav/LeftNav.jsx
@@ -27,7 +27,12 @@ const LeftNav = ({ open }) => {
         <a>My Queues</a>
       </li>
       <li>
-        <a tabIndex={-1} role="link" target="_blank" href="https://iimb.qualtrics.com/jfe/form/SV_aY8FY91ztRX9NvD">
+        <a
+          tabIndex={-1}
+          role="link"
+          target="_blank"
+          href="https://iimb.qualtrics.com/jfe/form/SV_aY8FY91ztRX9NvD"
+        >
           Contact Us
         </a>
       </li>
