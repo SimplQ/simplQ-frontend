c6d5e109924e29fc8bd3572d17090f668270ae39
daltonfury42 <daltonfury42@users.noreply.github.com>
2020-10-18 01:26:03 +0530

    Footer Margin Spacing and CSS (#382)

--- simplq/src/components/common/Footer.jsx
+++ simplq/src/components/common/Footer.jsx
@@ -1,89 +1,69 @@
 import React from 'react';
 import styles from '../../styles/footer.module.scss';
 import { ForkOnGithubButton } from './Button/Button.stories';
+import ClickableLogo from './ClickableLogo';
 
 export default () => (
   <div id="target_contact_us" className={styles['footer']}>
-    <div className={styles['main-card-container']}>
-      <div className={styles['card']}>
-        <div className={styles['card-header']}>
-          <div className={styles['simpleQ-image-container']}>
-            <img
-              src="/images/new_logo.svg"
-              className={styles['new-logo-image']}
-              alt="simplQ new logo"
-            />
-          </div>
-          <div className={styles['logo-header-container']}>
-            <h1>SimplQ</h1>
-          </div>
+    <div className={styles['card']}>
+      <ClickableLogo />
+      <div className={styles['card-body']}>
+        <div className={styles['description-content-container']}>
+          <b>
+            SimplQ is a completely web based queue management solution that anyone can use to create
+            instant virtual queues.
+            <br />
+            <a href="/privacy">Privacy Policy</a>
+            <br />
+            <a href="/privacy#terms-of-service">Terms of Service</a>
+          </b>
         </div>
-        <div className={styles['card-body']}>
-          <div className={styles['description-content-container']}>
+      </div>
+    </div>
+
+    <div className={styles['card']}>
+      <h1>Open source</h1>
+      <div className={styles['card-body']}>
+        <div className={styles['open-source-content-container']}>
+          <p>
             <b>
-              SimplQ is a completely web based queue management solution that anyone can use to
-              create instant virtual queues.
+              <span>SimplQ </span>
+              is open source. Be part of the
+              <span> SimplQ </span>
+              community.
             </b>
-          </div>
-        </div>
-        <a href="/privacy">Privacy Policy</a>
-        <a href="/privacy#terms-of-service">Terms of Service</a>
-      </div>
-
-      <div className={styles['card']}>
-        <div className={styles['card-header']}>
-          <div className={styles['center-horizontally']}>
-            {/* TOFIX: No such class / wrong approach */}
-            <h1>Open source</h1>
-          </div>
-        </div>
-        <div className={styles['card-body']}>
-          <div className={styles['open-source-content-container']}>
-            <p>
-              <b>
-                <span>SimplQ </span>
-                is open source. Be part of the
-                <span> SimplQ </span>
-                community.
-              </b>
-            </p>
-            <ForkOnGithubButton />
-          </div>
+          </p>
+          <ForkOnGithubButton />
         </div>
       </div>
+    </div>
 
-      <div className={styles['card']}>
-        <div className={styles['card-header']}>
-          <div className={styles['center-horizontally']}>
-            {/* TOFIX: No such class / wrong approach */}
-            <h1>Keep in touch</h1>
-          </div>
-        </div>
-        <div className={styles['card-body']}>
-          <div className={styles['list-container']}>
-            <ul>
-              <li>
-                <a href="https://iimb.qualtrics.com/jfe/form/SV_aY8FY91ztRX9NvD">Contact Us</a>
-              </li>
-              <li>
-                <a href="https://github.com/SimplQ/simplQ-frontend">Github</a>
-              </li>
-              <li>
-                <a href="https://medium.com/@raimazach/virtualizing-queues-a-long-overdue-alternative-to-physical-queues-bfdc4b51070f">
-                  Medium
-                </a>
-              </li>
-              <li>
-                <a href="https://www.youtube.com/channel/UCAb9PSXvrGZ4vvSneK1Nrow">Youtube</a>
-              </li>
-              <li>
-                <a href="https://www.facebook.com/simplq/">Facebook</a>
-              </li>
-              <li>
-                <a href="mailto:contact@simplq.me">Gmail</a>
-              </li>
-            </ul>
-          </div>
+    <div className={styles['card']}>
+      <h1>Keep in touch</h1>
+      <div className={styles['card-body']}>
+        <div className={styles['list-container']}>
+          <ul>
+            <li>
+              <a href="https://iimb.qualtrics.com/jfe/form/SV_aY8FY91ztRX9NvD">Contact Us</a>
+            </li>
+            <li>
+              <a href="https://github.com/SimplQ/simplQ-frontend">Github</a>
+            </li>
+            <li>
+              <a href="https://medium.com/@raimazach/virtualizing-queues-a-long-overdue-alternative-to-physical-queues-bfdc4b51070f">
+                Medium
+              </a>
+            </li>
+            <li>
+              <a href="https://www.youtube.com/channel/UCAb9PSXvrGZ4vvSneK1Nrow">Youtube</a>
+            </li>
+            <li>
+              <a href="https://www.facebook.com/simplq/">Facebook</a>
+            </li>
+            <li>
+              <a href="mailto:contact@simplq.me">Gmail</a>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
