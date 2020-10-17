ad754125fcbd30ec2cbfb5055eeff508aaa5c88c
daltonfury42 <daltonfury42@users.noreply.github.com>
2020-10-18 01:12:19 +0530

    Standardise Margin and Refactor CSS for Home page  (#378)

--- simplq/src/components/pages/Home/StaticInfos.jsx
+++ simplq/src/components/pages/Home/StaticInfos.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import styles from '../../../styles/homePage.module.scss';
 
 export const BenefitsInfo = () => (
-  <div className={styles['benefits-info']}>
+  <div className={`${styles['section']} ${styles['benefits-info']}`}>
     <img src="/images/corona.png" alt="background-illustration" className={styles['corona-1']} />
     <img src="/images/corona.png" alt="background-illustration" className={styles['corona-2']} />
     <img src="/images/corona.png" alt="background-illustration" className={styles['corona-3']} />
     <h2>Why SimplQ ?</h2>
-    <div className={styles['benefits-container']}>
+    <div className={`${styles['container']} ${styles['benefits-container']}`}>
       <div className={styles.benefit}>
         <img src="/images/minimize_crowding.svg" alt="mimimze crowding" />
         <p>No more waiting in long lines</p>
@@ -25,51 +25,52 @@ export const BenefitsInfo = () => (
 );
 
 export const HowToCreate = () => (
-  <div id="target_how_it_works" className={styles['queue-info']}>
-    <h2>How does SimplQ work?</h2>
-    <div className={styles['queue-container']}>
-      <div className={styles.queue}>
-        <img src="/images/queue_creation.svg" alt="Queue creation" />
-      </div>
-      <div className={styles.queue}>
-        <ul className={styles['list-info']}>
-          <img src="/images/wand.svg" alt="Wand" />
-          <li>
-            <h3>Create a Queue</h3>
-            <p>Enter a queue name and click on the create queue button to generate a new queue</p>
-          </li>
-          <img src="/images/share-24px.svg" alt="Share" />
-          <li>
-            <h3>Share a queue link</h3>
-            <p>
-              Click on the copy to clipboard button and share the queue link with your users to join
-            </p>
-          </li>
-          <img src="/images/people-24px.svg" alt="People" />
-          <li>
-            <h3>Manage your queue</h3>
-            <p>
-              Manage your queues seamlessly and notify users from the dashboard when their turn is
-              up
-            </p>
-          </li>
-        </ul>
+  <>
+    <div id="target_how_it_works" className={`${styles['section']} ${styles['queue-info']}`}>
+      <h2>Create a Queue at the click of a button</h2>
+      <div className={`${styles['container']} ${styles['queue-container']}`}>
+        <div className={styles.queue}>
+          <img src="/images/queue_creation.svg" alt="Queue creation" />
+        </div>
+        <div className={styles.queue}>
+          <ul className={styles['list-info']}>
+            <img src="/images/wand.svg" alt="Wand" />
+            <li>
+              <h3>Create Queue</h3>
+              <p>
+                Enter queue name of your choice and create a brand new queue totally free of cost
+              </p>
+            </li>
+            <img src="/images/share-24px.svg" alt="Share" />
+            <li>
+              <h3>Share queue</h3>
+              <p>Click the share button and send the queue link to your users for them to join</p>
+            </li>
+            <img src="/images/people-24px.svg" alt="People" />
+            <li>
+              <h3>Manage queue</h3>
+              <p>
+                Manage your queues seamlessly and efficiently to provide a better user experience
+              </p>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 50">
+    <svg id={styles['curved-div-2']} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 50">
       <path
         fill="#6C63FF29"
         fillOpacity="1"
         d="M0,32L120,26.7C240,21,480,11,720,10.7C960,11,1200,21,1320,26.7L1440,32L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"
       />
     </svg>
-  </div>
+  </>
 );
 
 export const HowToJoin = () => (
-  <div className={styles['join-info']}>
-    <h2>How do people join a queue?</h2>
-    <div className={styles['queue-container']}>
+  <div className={`${styles['section']} ${styles['join-info']}`}>
+    <h2>Join a queue without being there</h2>
+    <div className={`${styles['container']} ${styles['queue-container']}`}>
       <div className={styles['queue']}>
         <ul className={styles['list-info']}>
           <img src="/images/mobile_friendly-24px.svg" alt="Mobile friendly" />
@@ -92,28 +93,30 @@ export const HowToJoin = () => (
 );
 
 export const ExtraInfo = () => (
-  <div className={styles['extra-info']}>
-    <div className={styles['card']}>
-      <img src="/images/free.svg" alt="free" />
-      <p className={styles['brief-description']}>Always free</p>
-      <p className={styles['detailed-description']}>
-        We are a team of enthusiastic developers who want to give back to society and do not
-        prioritize monetary gains
-      </p>
-    </div>
-    <div className={styles['card']}>
-      <img src="/images/secure.svg" alt="secure" />
-      <p className={styles['brief-description']}>Data security</p>
-      <p className={styles['detailed-description']}>
-        Security is paramount and we take data security seriously
-      </p>
-    </div>
-    <div className={styles['card']}>
-      <img src="/images/easy.svg" alt="easy to use" />
-      <p className={styles['brief-description']}>Easy to use</p>
-      <p className={styles['detailed-description']}>
-        We provide free queue management thereby allowing you to focus on other things
-      </p>
+  <div className={`${styles['section']} ${styles['extra-info']}`}>
+    <div className={`${styles['container']} ${styles['extra-container']}`}>
+      <div className={styles['card']}>
+        <img src="/images/free.svg" alt="free" />
+        <p className={styles['brief-description']}>Always free</p>
+        <p className={styles['detailed-description']}>
+          We are a team of enthusiastic developers who want to give back to society and do not
+          prioritize monetary gains
+        </p>
+      </div>
+      <div className={styles['card']}>
+        <img src="/images/secure.svg" alt="secure" />
+        <p className={styles['brief-description']}>Data security</p>
+        <p className={styles['detailed-description']}>
+          Security is paramount and we take data security seriously
+        </p>
+      </div>
+      <div className={styles['card']}>
+        <img src="/images/easy.svg" alt="easy to use" />
+        <p className={styles['brief-description']}>Easy to use</p>
+        <p className={styles['detailed-description']}>
+          We provide free queue management thereby allowing you to focus on better things
+        </p>
+      </div>
     </div>
   </div>
 );
