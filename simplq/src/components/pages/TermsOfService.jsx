c34729bd42cdbd9e24ad1bb36b811d3e5080286a
daltonfury42 <daltonfury42@disroot.org>
2020-10-17 18:10:39 +0530

    add space

--- simplq/src/components/pages/TermsOfService.jsx
+++ simplq/src/components/pages/TermsOfService.jsx
@@ -12,7 +12,7 @@ function TermsOfService(props) {
         <h2>Privacy Policy</h2>
         <p>
           Your privacy is important to us. It is SimplQ&apos;s policy to respect your privacy
-          regarding any information we may collect from you across our website,
+          regarding any information we may collect from you across our website,&nbsp;
           <a href="http://simplq.me">http://simplq.me</a>
           <span>, and other sites we own and operate.</span>
         </p>
@@ -49,7 +49,7 @@ function TermsOfService(props) {
         <h2>SimplQ Terms of Service</h2>
         <h3>1. Terms</h3>
         <p>
-          By accessing the website at
+          By accessing the website at&nbsp;
           <a href="http://simplq.me">http://simplq.me</a>
           <span>
             , you are agreeing to be bound by these terms of service, all applicable laws and
