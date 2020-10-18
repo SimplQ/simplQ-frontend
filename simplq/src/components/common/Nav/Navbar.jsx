e75a2f81851722c128ddb1690e0a43ca5231a255
daltonfury42 <daltonfury42@disroot.org>
2020-10-19 00:29:04 +0530

    fix merge c

--- simplq/src/components/common/Nav/Navbar.jsx
+++ simplq/src/components/common/Nav/Navbar.jsx
@@ -2,17 +2,12 @@ import React from 'react';
 import styles from '../../../styles/navbar.module.scss';
 import Burger from './Burger';
 import Logo from '../ClickableLogo';
-import { smoothScrollTo } from '../utilFns';
+import { smoothScrollToHomePageTop } from '../utilFns';
 
 const Navbar = () => {
-  const scrollToTop = () => {
-    const element = document.getElementById('target_top');
-    smoothScrollTo(element);
-  };
-
   return (
     <nav className={styles['navbar']}>
-      <Logo onClick={scrollToTop} />
+      <Logo onClick={smoothScrollToHomePageTop} />
       <Burger />
     </nav>
   );
