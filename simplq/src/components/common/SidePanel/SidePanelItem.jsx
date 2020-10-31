1fe3949e48a6b82eb208ea7698b34ac554276b61
daltonfury42 <daltonfury42@users.noreply.github.com>
2020-10-31 22:35:22 +0530

    AdminLayout2: Fix sidepanel (#402)

--- simplq/src/components/common/SidePanel/SidePanelItem.jsx
+++ simplq/src/components/common/SidePanel/SidePanelItem.jsx
@@ -1,17 +1,11 @@
 import React, { useState } from 'react';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { Collapse } from '@material-ui/core';
-import PropagateLoader from 'react-spinners/PropagateLoader';
 import styles from '../../../styles/sidePanel.module.scss';
+import LoadingIndicator from '../LoadingIndicator';
 
-const ExpandButton = ({ isOpen, onToggle }) => (
-  <div
-    role="button"
-    tabIndex={0}
-    className={styles['expand-button']}
-    onClick={onToggle}
-    onKeyDown={onToggle}
-  >
+const ExpandButton = ({ isOpen }) => (
+  <div className={styles['expand-button']}>
     <ExpandMoreIcon
       className={
         isOpen
@@ -33,30 +27,43 @@ const SidePanelItem = ({
   children,
 }) => {
   const [open, setOpen] = useState(false);
-  const toggleOpen = () => setOpen(!open);
+  const onClickHandler = () => {
+    if (expandable) {
+      setOpen(!open);
+    }
+    if (onClick) {
+      onClick();
+    }
+  };
 
   return (
-    <button type="button" className={styles['side-panel-item']} onClick={onClick}>
-      <div className={styles['header']}>
+    <div className={styles['side-panel-item']}>
+      <div
+        className={styles['header']}
+        tabIndex={0}
+        role="button"
+        onKeyDown={onClickHandler}
+        onClick={onClickHandler}
+      >
         <Icon className={styles['icon']} fontSize="large" />
         <div>
           <h2>{title}</h2>
           <p className={styles['description']}>{description}</p>
         </div>
-        {expandable && <ExpandButton isOpen={open} onToggle={toggleOpen} />}
+        {expandable && <ExpandButton isOpen={open} />}
       </div>
       {expandable && (
         <Collapse timeout="auto" in={open}>
           {loading ? (
             <div className={styles['loading']}>
-              <PropagateLoader color="#3a3768" />
+              <LoadingIndicator />
             </div>
           ) : (
             children
           )}
         </Collapse>
       )}
-    </button>
+    </div>
   );
 };
 
