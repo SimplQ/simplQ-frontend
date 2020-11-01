800270469d4b3d6db12a38a1bc3f2f00d61a85e2
daltonfury42 <daltonfury42@users.noreply.github.com>
2020-11-01 19:38:47 +0530

    Fix Join/Status Page Margins and Footer  (#405)

--- simplq/src/components/Layout.jsx
+++ simplq/src/components/Layout.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import Routes from './Routes';
 import Footer from './common/Footer';
+import styles from '../styles/layout.module.scss';
 
 function Layout() {
   return (
-    <>
-      <Routes />
-      <Footer />
-    </>
+    <div className={styles['box']}>
+      <div className={styles['content']}>
+        <Routes />
+      </div>
+      <div className={styles['footer']}>
+        <Footer />
+      </div>
+    </div>
   );
 }
 
