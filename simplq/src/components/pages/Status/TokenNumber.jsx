e9d4f330d8cad28f7769cf9aaeeb97ec37c23d8a
Rahul Patel <33618437+rahulpatel596@users.noreply.github.com>
2020-11-22 22:47:35 -0600

    Status Page Token Number Display Enhancement  (#420)

--- simplq/src/components/pages/Status/TokenNumber.jsx
+++ simplq/src/components/pages/Status/TokenNumber.jsx
@@ -3,7 +3,13 @@ import styles from '../../../styles/statusPage.module.scss';
 
 export default (props) => (
   <div className={styles['token-number']}>
-    <p>Token No: </p>
-    <p className={styles['count']}>{props.tokenNumber}</p>
+    <div className={styles['hanging-threads']} />
+    <div className={styles['token-container']}>
+      <span className={styles['token']}>Token No</span>
+    </div>
+    <div className={styles['separator']} />
+    <div className={styles['count-container']}>
+      <span className={styles['count']}>{props.tokenNumber}</span>
+    </div>
   </div>
 );
