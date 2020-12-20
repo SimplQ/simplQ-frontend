538d0efc2e587acd275b909633f1bb1f980c9a36
Dalton Fury <daltonfury42@gmail.com>
2020-12-20 20:55:08 +0530

    comments

--- simplq/src/services/auth.js
+++ simplq/src/services/auth.js
@@ -4,15 +4,19 @@ import { setIsLoggedIn, setMyQueues } from '../store/appSlice';
 let resolveGoogleUser = null;
 let googleUserPromise = null;
 
+/**
+ * We initilise googleUserPromise to a Promise and resolve it inside logIn() and logOut() functions below.
+ * This is required as otherwise a request might be send to the backend before the login
+ * completes, and the call will fail. This will allow us to wait till the promise resolves.
+ * This function should be called before every login attempt, as well as at app start so that a new
+ * promise is initilised.
+ * https://stackoverflow.com/questions/26150232/resolve-javascript-promise-outside-function-scope
+ */
 export function init() {
   googleUserPromise = new Promise(function (resolve) {
     resolveGoogleUser = resolve;
   });
 }
-
-/**
- * We initilise googleUserPromise to a Promise and resolve it inside logIn() and logOut(). It is initilised to a new pending promise at start.
- */
 init();
 
 export function isLoggedIn() {
@@ -37,7 +41,7 @@ export function logOut() {
 }
 
 /**
- * Below functions will crash if called when user is not logged in, as googleUser will be null.
+ * Below functions should be called only if logged in, otherwise googleUser will resolve to null.
  */
 export function getName() {
   return googleUserPromise.then((googleUser) => googleUser.getBasicProfile().getName());
