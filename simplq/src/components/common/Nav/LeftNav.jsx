c9c8e2e59f1949817e5104f998a6c88b6d3dc343
daltonfury42 <daltonfury42@users.noreply.github.com>
2020-12-25 22:36:00 +0530

    Add navbar to all Pages, nav minor bugfixes (#441)

--- simplq/src/components/common/Nav/LeftNav.jsx
+++ simplq/src/components/common/Nav/LeftNav.jsx
@@ -3,34 +3,34 @@
 import React from 'react';
 import styles from './nav.module.scss';
 import { smoothScrollTo } from '../utilFns';
-
 import LoginButton from '../LoginButton';
 
-const LeftNav = ({ open, toggleClose, page }) => {
+const LeftNav = ({ open, toggleClose }) => {
   const scrollToHowItWorks = () => {
     // Close the navbar on click
     toggleClose();
     // get the target div by ID
     const element = document.getElementById('target_how_it_works');
-    smoothScrollTo(element);
+    if (element) {
+      // element is on the current page, just have to scroll to it
+      smoothScrollTo(element);
+    } else {
+      window.location.href = '/#target_how_it_works';
+    }
   };
   return (
     <div>
       <ul className={styles['left-nav']} open={open}>
-        {page === 'home' ? (
-          <li>
-            <a tabIndex={0} role="link" onKeyDown={scrollToHowItWorks} onClick={scrollToHowItWorks}>
-              How it works
-            </a>
-          </li>
-        ) : null}
-        {page === 'admin' ? (
-          <li>
-            <a tabIndex={0} href="/">
-              My Queues
-            </a>
-          </li>
-        ) : null}
+        <li>
+          <a tabIndex={0} href="/">
+            Home
+          </a>
+        </li>
+        <li>
+          <a role="link" tabIndex={0} onKeyDown={scrollToHowItWorks} onClick={scrollToHowItWorks}>
+            How it Works
+          </a>
+        </li>
         <li>
           <a tabIndex={0} href="https://iimb.qualtrics.com/jfe/form/SV_aY8FY91ztRX9NvD">
             Contact Us
