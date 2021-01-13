4d7eec963d305d37e7e3cefe8a2a595e6fe5b7d2
Bhargava Prabu Reddy <44055337+prabureddy@users.noreply.github.com>
2021-01-13 23:53:17 +0530

    Added close button to QrCode Modal and Optimized the mobile view (#460)

--- simplq/src/components/common/Popup/QrCode.jsx
+++ simplq/src/components/common/Popup/QrCode.jsx
@@ -1,7 +1,8 @@
-import React, { useState, useRef, forwardRef } from 'react';
+import React, { useRef, forwardRef } from 'react';
 import { makeStyles, Modal, useMediaQuery } from '@material-ui/core';
 import { QRCode } from 'react-qrcode-logo';
 import PrintIcon from '@material-ui/icons/Print';
+import HighlightOffIcon from '@material-ui/icons/HighlightOff';
 import { useReactToPrint } from 'react-to-print';
 import StandardButton from '../Button';
 import { title } from '../utilFns';
@@ -14,35 +15,34 @@ const ComponentToPrint = forwardRef(({ style, url, queueName }, ref) => {
       </h1>
       <h2>Scan this QR to begin!</h2>
       <QRCode value={url} />
-      <p>{`or visit ${url}`}</p>
+      <p style={{ textAlign: 'center' }}>{`or visit ${url}`}</p>
     </div>
   );
 });
 
 const QrCode = (props) => {
   const { queueName, show, onClose } = props;
-  const isMobile = useMediaQuery((theme) => theme.breakpoints.down('xs'));
 
-  const getModalStyle = () => {
-    return {
-      position: 'absolute',
-      top: '20%',
-      backgroundColor: '#fff',
-      borderRadius: '25px',
-      outline: 'none',
-      width: isMobile ? '50%' : 400,
-      padding: 10,
-    };
-  };
+  const isMobile = useMediaQuery((theme) => theme.breakpoints.down('sm'));
 
-  const [modalStyle] = useState(getModalStyle);
   const componentPrintRef = useRef();
+  
   const handlePrint = useReactToPrint({
     content: () => componentPrintRef.current,
   });
 
   const styles = makeStyles(() => {
     return {
+      modalContainer: {
+        position: 'absolute',
+        top: '20%',
+        backgroundColor: '#fff',
+        borderRadius: '25px',
+        outline: 'none',
+        width: isMobile ? '95%' : 400,
+        padding: 10,
+        margin: isMobile ? 10 : 0,
+      },
       centered: {
         display: 'flex',
         alignItems: 'center',
@@ -51,8 +51,16 @@ const QrCode = (props) => {
         width: '100%',
         height: '80%',
       },
+      actionContainer: {
+        display: 'flex',
+        justifyContent: 'space-evenly',
+        width: '90%',
+      },
     };
   })();
+
+  const handleModalClose = () => onClose(false);
+
   return (
     <Modal
       style={{
@@ -62,9 +70,9 @@ const QrCode = (props) => {
         justifyContent: 'center',
       }}
       open={queueName ? show : !show}
-      onClose={() => onClose(false)}
+      onClose={handleModalClose}
     >
-      <div style={modalStyle}>
+      <div className={styles['modalContainer']}>
         <div className={styles['centered']}>
           <ComponentToPrint
             style={styles['centered']}
@@ -72,9 +80,14 @@ const QrCode = (props) => {
             queueName={queueName}
             ref={componentPrintRef}
           />
-          <StandardButton onClick={handlePrint} icon={<PrintIcon />}>
-            Print
-          </StandardButton>
+          <div className={styles['actionContainer']}>
+            <StandardButton onClick={handlePrint} icon={<PrintIcon />}>
+              Print
+            </StandardButton>
+            <StandardButton onClick={handleModalClose} icon={<HighlightOffIcon />} outlined>
+              Close
+            </StandardButton>
+          </div>
         </div>
       </div>
     </Modal>
