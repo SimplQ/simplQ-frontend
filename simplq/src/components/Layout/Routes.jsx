b18e4c78777fbc39bdccaa0501ed4b2ae30023c9
Dalton Fury <daltonfury42@gmail.com>
2021-01-23 19:32:08 +0530

    Initialize Okta

--- simplq/src/components/Layout/Routes.jsx
+++ simplq/src/components/Layout/Routes.jsx
@@ -9,21 +9,34 @@ import PopupNotifications from '../common/Popup';
 import Home from '../pages/Home';
 import TermsOfService from '../pages/TermsOfService';
 import Navbar from '../common/Nav/Navbar';
+import { Security, LoginCallback  } from '@okta/okta-react';
+import { OktaAuth } from '@okta/okta-auth-js';
+
+const config = {
+  issuer: 'https://dev-98826368.okta.com/oauth2/default',
+  clientId: "0oa48de0mFaP5PGG05d6",
+  redirectUri: 'http://' + window.location.host+'/login/callback',
+  scopes: 'openid profile email'.split(/\s+/)
+};
+const oktaAuth = new OktaAuth(config);
 
 export default () => {
   return (
     <>
       <ErrorBoundary>
         <Router>
-          <Navbar />
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/queue/:queueId" exact component={AdminQueue} />
-            <Route path="/j/:queueName" exact component={JoinQueueWithDetails} />
-            <Route path="/token/:tokenId" exact component={QueueStatus} />
-            <Route path="/privacy" exact component={TermsOfService} />
-            <Route component={PageNotFound} />
-          </Switch>
+          <Security oktaAuth={oktaAuth}>
+            <Navbar />
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/queue/:queueId" exact component={AdminQueue} />
+              <Route path="/j/:queueName" exact component={JoinQueueWithDetails} />
+              <Route path="/token/:tokenId" exact component={QueueStatus} />
+              <Route path="/privacy" exact component={TermsOfService} />
+              <Route path="/login/callback" component={LoginCallback}/>
+              <Route component={PageNotFound} />
+            </Switch>
+          </Security>
         </Router>
         <PopupNotifications />
       </ErrorBoundary>
--- simplq/src/components/common/Nav/Navbar.jsx
+++ simplq/src/components/common/Nav/Navbar.jsx
@@ -3,8 +3,15 @@ import styles from './nav.module.scss';
 import Burger from './Burger';
 import Logo from '../ClickableLogo';
 import { smoothScrollToHomePageTop } from '../utilFns';
+import { useOktaAuth } from '@okta/okta-react';
 
 export const Navbar = () => {
+  const { authState, authService } = useOktaAuth();
+  const login = () => authService.login();
+
+  console.log("authState: ", authState);
+  console.log("authService: ", authService)
+  
   return (
     <nav className={styles['navbar']}>
       <Logo onClick={smoothScrollToHomePageTop} />
