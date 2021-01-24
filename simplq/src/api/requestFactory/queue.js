ad3c8d4d3e0ae853c97d2e70aae186da7cccaf88
Dalton Fury <daltonfury42@gmail.com>
2021-01-25 00:29:43 +0530

    Slimmer syntax

--- simplq/src/api/requestFactory/queue.js
+++ simplq/src/api/requestFactory/queue.js
@@ -1,21 +1,14 @@
-export const getMyQueues = () => {
-  return { method: 'get', url: '/queues' };
-};
+export const getMyQueues = () => ({ method: 'get', url: '/queues' });
 
 export const create = (queueName) => ({ method: 'post', url: '/queue', data: { queueName } });
 
-export const get = (queueId) => {
-  return { method: 'get', url: `/queue/${queueId}` };
-};
+export const get = (queueId) => ({ method: 'get', url: `/queue/${queueId}` });
 
-export const getStatus = (queueId) => {
-  return { method: 'get', url: `/queue/status?queueId=${queueId}` };
-};
+export const getStatus = (queueId) => ({ method: 'get', url: `/queue/status?queueId=${queueId}` });
 
-export const getStatusByName = (queueName) => {
-  return { method: 'get', url: `/queue/status?queueName=${queueName}` };
-};
+export const getStatusByName = (queueName) => ({
+  method: 'get',
+  url: `/queue/status?queueName=${queueName}`,
+});
 
-export const deleteQueue = (queueId) => {
-  return { method: 'delete', url: `/queue/${queueId}` };
-};
+export const deleteQueue = (queueId) => ({ method: 'delete', url: `/queue/${queueId}` });
--- simplq/src/api/requestFactory/token.js
+++ simplq/src/api/requestFactory/token.js
@@ -1,24 +1,16 @@
-export const create = (name, contact, notifiable, queueId) => {
-  return {
-    method: 'post',
-    url: '/token',
-    data: {
-      name,
-      contactNumber: contact,
-      queueId,
-      notifiable,
-    },
-  };
-};
+export const create = (name, contact, notifiable, queueId) => ({
+  method: 'post',
+  url: '/token',
+  data: {
+    name,
+    contactNumber: contact,
+    queueId,
+    notifiable,
+  },
+});
 
-export const get = (tokenId) => {
-  return { method: 'get', url: `/token/${tokenId}` };
-};
+export const get = (tokenId) => ({ method: 'get', url: `/token/${tokenId}` });
 
-export const notify = (tokenId) => {
-  return { method: 'put', url: `/token/notify/${tokenId}` };
-};
+export const notify = (tokenId) => ({ method: 'put', url: `/token/notify/${tokenId}` });
 
-export const remove = (tokenId) => {
-  return { method: 'delete', url: `/token/${tokenId}` };
-};
+export const remove = (tokenId) => ({ method: 'delete', url: `/token/${tokenId}` });
