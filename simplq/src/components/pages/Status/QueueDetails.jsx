447dcb9b36d6e1e889c0971bf3ae6c1a2ddba811
Mischa Radenovic <miroslav.radenovic@gmail.com>
2021-02-04 17:07:41 -0500

    Fix: Unwrap Result before using it

--- simplq/src/components/pages/Status/QueueDetails.jsx
+++ simplq/src/components/pages/Status/QueueDetails.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useCallback } from 'react';
+import { unwrapResult } from '@reduxjs/toolkit';
 import InfoIcon from '@material-ui/icons/Info';
 import QueueStats from 'components/common/QueueStats';
 import { useGetQueueStatus } from 'store/queues';
@@ -12,7 +13,11 @@ export default ({ queueId }) => {
 
   useEffect(() => {
     // TODO: Can this state be reused? Do we need a reducer for it?
-    dispatch(getQueueStatus(queueId)).then((status) => setQueueStatus(status));
+    dispatch(getQueueStatus({ queueId }))
+      .then(unwrapResult)
+      .then((status) => {
+        setQueueStatus(status);
+      });
   }, [queueId, dispatch, getQueueStatus]);
 
   return (
