ffa5777eca40e7ed625c8c4566f5297d239cab9e
Mischa Radenovic <miroslav.radenovic@gmail.com>
2021-02-08 11:51:37 -0500

    Pass async action to LoadingStatus

--- simplq/src/components/common/Loading/LoadingStatus.jsx
+++ simplq/src/components/common/Loading/LoadingStatus.jsx
@@ -3,7 +3,8 @@ import { useSelector } from 'react-redux';
 import Loading from './Loading';
 
 export default ({ children, dependsOn }) => {
-  const actionStatus = useSelector((state) => state.actionStatus[dependsOn]);
+  const actionName = Object.keys(dependsOn)[0];
+  const actionStatus = useSelector((state) => state.actionStatus[actionName]);
 
   return <Loading actionStatus={actionStatus}>{children}</Loading>;
 };
