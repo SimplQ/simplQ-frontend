5ecd48b5fabf228edf845ed4f6942c9f521ce42e
daltonfury42 <daltonfury42@users.noreply.github.com>
2021-02-14 00:24:26 +0530

    loaded flag in store, refactor (#527)
    Refactor to fetch from store where data is needed, than to pass it down from main component

--- simplq/src/components/pages/Status/TokenNumber.jsx
+++ simplq/src/components/pages/Status/TokenNumber.jsx
@@ -1,15 +1,24 @@
+import LoadingIndicator from 'components/common/LoadingIndicator';
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectToken } from 'store/token';
 import styles from './status.module.scss';
 
-export default (props) => (
-  <div className={styles['token-number']}>
-    <div className={styles['hanging-threads']} />
-    <div className={styles['token-container']}>
-      <span className={styles['token']}>Token No</span>
+export default () => {
+  const { token, loading } = useSelector(selectToken);
+
+  return (
+    <div className={styles['token-number']}>
+      <div className={styles['hanging-threads']} />
+      <div className={styles['token-container']}>
+        <span className={styles['token']}>Token No</span>
+      </div>
+      <div className={styles['separator']} />
+      <div className={styles['count-container']}>
+        <span className={styles['count']}>
+          {loading ? <LoadingIndicator /> : token.tokenNumber}
+        </span>
+      </div>
     </div>
-    <div className={styles['separator']} />
-    <div className={styles['count-container']}>
-      <span className={styles['count']}>{props.tokenNumber}</span>
-    </div>
-  </div>
-);
+  );
+};
