7e8c77088cf18ea66a0de918c127edbc49281ace
Mischa Radenovic <mradenovic@users.noreply.github.com>
2021-02-19 09:39:37 -0500

    Mock auth0 in storybook (#546)
    * Mock auth0 in storybook
    
    This allows components that depend on auth0 to be in developed
    in isolation.
    
    * Fix code style issues with Prettier
    
    Co-authored-by: Lint Action <lint-action@samuelmeuli.com>

--- simplq/.storybook/preview.js
+++ simplq/.storybook/preview.js
@@ -1,6 +1,7 @@
 import reduxDecorator from '__mocks__/reduxDecorator';
+import auth0Decorator from '__mocks__/auth0';
 
-export const decorators = [reduxDecorator];
+export const decorators = [reduxDecorator, auth0Decorator];
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
 };
