8db703290d479da4cd5a4550aa46a68faef993ea
Nithin <nithinjosetom@gmail.com>
2021-02-21 18:09:20 +0530

    Refactored delete queue (#551)

--- simplq/src/store/asyncActions/deleteQueue.js
+++ simplq/src/store/asyncActions/deleteQueue.js
@@ -1,6 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { useMakeAuthedRequest } from 'api/auth';
 import * as RequestFactory from 'api/requestFactory';
+import { useHistory } from 'react-router';
 
 const typePrefix = 'deleteQueue/action';
 
@@ -11,11 +12,15 @@ const typePrefix = 'deleteQueue/action';
  */
 const useDeleteQueue = () => {
   const makeAuthedRequest = useMakeAuthedRequest();
+  const history = useHistory();
 
   const deleteQueue = createAsyncThunk(typePrefix, async (arg) => {
-    const { queueId } = arg;
+    const { queueId, goHome } = arg;
     const authedRequest = makeAuthedRequest(RequestFactory.deleteQueue(queueId));
     const response = await authedRequest;
+    if (goHome) {
+      history.push('/');
+    }
     return response;
   });
 
