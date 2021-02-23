6dcc5419c2019ddf77532d19ce1f3b8d4a85b368
maaverik <nithinjosetom@gmail.com>
2021-02-23 22:06:56 +0530

    Moved getUserQueues dispatch to LandingPage to prevent unneeded popup on other page loads

--- simplq/src/components/Layout/Layout.jsx
+++ simplq/src/components/Layout/Layout.jsx
@@ -1,24 +1,12 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import Footer from 'components/common/Footer';
 import Loading from 'components/common/Loading/Loading';
-import { useGetUserQueues } from 'store/asyncActions';
-import { useDispatch } from 'react-redux';
 import Routes from './Routes';
 import styles from './Layout.module.scss';
 
 function Layout() {
-  const { isLoading, isAuthenticated } = useAuth0();
-  const dispatch = useDispatch();
-  const getUserQueues = useGetUserQueues();
-
-  useEffect(() => {
-    // TODO: Server still returns queues for anonymous users
-    //  Dispatch action only if auth is loaded
-    if (isLoading === false) {
-      dispatch(getUserQueues());
-    }
-  }, [isLoading, isAuthenticated, getUserQueues, dispatch]);
+  const { isLoading } = useAuth0();
 
   return (
     <div className={styles['box']}>
