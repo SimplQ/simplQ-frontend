ed068be04ab0ed9e4432a9f6f4d4c91d8ae7e607
maaverik <nithinjosetom@gmail.com>
2021-02-23 22:02:27 +0530

    Moved token selector to tokenlist

--- simplq/src/components/pages/Admin/TokenList.jsx
+++ simplq/src/components/pages/Admin/TokenList.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import Loading from 'components/common/Loading/Loading';
+import { useSelector } from 'react-redux';
+import { selectTokens } from 'store/selectedQueue';
 import Token from './Token';
 import styles from './admin.module.scss';
 
-function TokenList({ tokens }) {
-  const Dispaly = () =>
+function TokenList() {
+  const tokens = useSelector(selectTokens);
+  const ListContent = () =>
     tokens.length === 0 ? (
       <p>Your queue has been created and is currently empty. Waiting for people to join...</p>
     ) : (
       tokens.map((token) => <Token token={token} key={token.tokenId} />)
     );
+  console.log('In token page');
 
   return (
     <Loading isLoading={tokens === undefined}>
       <div className={styles['token-list']}>
-        <Dispaly />
+        <ListContent />
       </div>
     </Loading>
   );
