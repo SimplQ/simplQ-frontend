94a3c93a41e9404bdd4ae177466c88e5f84fcef6
kabaliisa <kabaliisa15@gmail.com>
2021-03-04 09:20:15 +0300

    Fix return statement to immediately  expression

--- simplq/src/store/asyncActions/createQueue.js
+++ simplq/src/store/asyncActions/createQueue.js
@@ -14,7 +14,7 @@ const useCreateQueue = () => {
   const makeAuthedRequest = useMakeAuthedRequest();
   const history = useHistory();
 
-  const createQueue = createAsyncThunk(typePrefix, async ({ queueName }) => {
+  return createAsyncThunk(typePrefix, async ({ queueName }) => {
     const authedRequest = makeAuthedRequest(RequestFactory.createQueue(queueName));
     const response = await authedRequest;
     if (response) {
@@ -22,8 +22,6 @@ const useCreateQueue = () => {
     }
     return response;
   });
-
-  return createQueue;
 };
 
 const createQueue = createAsyncThunk(typePrefix);
