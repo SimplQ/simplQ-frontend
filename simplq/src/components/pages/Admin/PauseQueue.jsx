5f535bb103c6f1c9394e1239ceb03865ba4b5fad
Manav Mendiratta <mmendiratta@wisc.edu>
2021-03-14 13:12:08 -0500

    small change to get an idea of what to work with

--- simplq/src/components/pages/Admin/PauseQueue.jsx
+++ simplq/src/components/pages/Admin/PauseQueue.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PauseIcon from '@material-ui/icons/Pause';
 import SidePanelItem from 'components/common/SidePanel/SidePanelItem';
 
 export default () => {
+  const [description, setDescription] = useState('Temporarily stop people from joining');
+  const [paused, setPaused] = useState(false);
+
+  // TODO: clean up this implementation
+  const getPausedOptions = () => {
+    if (!paused) {
+      setDescription('Start allowing people to join the queue');
+      setPaused(true);
+    } else {
+      setDescription('Temporarily stop people from joining');
+      setPaused(false);
+    }
+  };
+
   return (
     <SidePanelItem
       Icon={PauseIcon}
       title="Pause Queue"
-      description="Temporarily stop people from joining"
+      description={description}
+      onClick={() => getPausedOptions()}
     />
   );
 };
