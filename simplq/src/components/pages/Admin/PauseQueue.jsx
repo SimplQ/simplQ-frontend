680351cc9b0ac361b523dbf2074ff02234f1506b
Manav Mendiratta <mmendiratta@wisc.edu>
2021-03-15 21:31:00 -0500

    panel changes based off admin choice for status of queue

--- simplq/src/components/pages/Admin/PauseQueue.jsx
+++ simplq/src/components/pages/Admin/PauseQueue.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import PauseIcon from '@material-ui/icons/Pause';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 import SidePanelItem from 'components/common/SidePanel/SidePanelItem';
 import { usePauseQueue } from 'store/asyncActions/pauseQueue';
 
@@ -9,32 +10,29 @@ export default ({ queueId }) => {
   const pauseQueue = usePauseQueue();
 
   const [description, setDescription] = useState('Temporarily stop people from joining');
-  const [paused, setPaused] = useState(false);
+  const [isActive, setIsActive] = useState(false);
 
-  const onPauseClick = (chosenStatus) => {
-    console.log('dispatch for pause: ', chosenStatus);
+  const onPanelClick = (chosenStatus) => {
     dispatch(pauseQueue({ queueId, status: chosenStatus }));
   };
 
   // TODO: clean up this implementation
   const getPausedOptions = () => {
-    if (!paused) {
-      console.log('Pause click');
+    if (!isActive) {
       setDescription('Start allowing people to join the queue');
-      onPauseClick('PAUSED');
-      setPaused(true);
+      onPanelClick('PAUSED');
+      setIsActive(true);
     } else {
-      console.log('Active click');
       setDescription('Temporarily stop people from joining');
-      onPauseClick('ACTIVE');
-      setPaused(false);
+      onPanelClick('ACTIVE');
+      setIsActive(false);
     }
   };
 
   return (
     <SidePanelItem
-      Icon={PauseIcon}
-      title="Pause Queue"
+      Icon={isActive ? PlayArrowIcon : PauseIcon}
+      title={isActive ? 'Resume Queue' : 'Pause Queue'}
       description={description}
       onClick={getPausedOptions}
     />
