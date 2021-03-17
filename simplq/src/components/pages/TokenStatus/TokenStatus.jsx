32dbf5465ee6dd9cb3047df693d27f123d01d1a1
Manav Mendiratta <mmendiratta@wisc.edu>
2021-03-16 20:01:20 -0500

    the queue status is checked and updates the user, issue with the rendering, state of queue is behind

--- simplq/src/components/pages/TokenStatus/TokenStatus.jsx
+++ simplq/src/components/pages/TokenStatus/TokenStatus.jsx
@@ -1,25 +1,46 @@
-import React from 'react';
+import React, { useEffect, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useGetToken } from 'store/asyncActions';
+import { useGetToken, useGetQueueStatus } from 'store/asyncActions';
 import { selectToken } from 'store/token';
+
+import { selectQueueStatus } from 'store/queueStatus';
 import LoadingStatus from 'components/common/Loading/LoadingStatus';
 import Button from 'components/common/Button';
 import styles from './status.module.scss';
 
 export default () => {
   const token = useSelector(selectToken);
+  const queueStatus = useSelector(selectQueueStatus);
   const dispatch = useDispatch();
   const getToken = useGetToken();
+  const getQueueStatus = useCallback(useGetQueueStatus(), []);
+
+  useEffect(async () => {
+    await getQueueStatus({ queueId: token.queueId });
+  }, [getQueueStatus]);
 
   const onRefreshClick = () => {
     dispatch(getToken({ tokenId: token.tokenId }));
   };
 
   let status = null;
+  // TODO: IMPLEMENTATION OF CORRECT RENDERING
   if (token.tokenStatus === 'REMOVED') {
     status = <p>You have been removed from the queue, have a nice day</p>;
   } else if (token.tokenStatus === 'NOTIFIED') {
     status = <p>Your turn is up, please proceed to the counter</p>;
+  } else if (queueStatus.status === 'PAUSED') {
+    /* eslint-disable react/jsx-one-expression-per-line */
+    status = (
+      <>
+        <p>Hello{token.name},</p>
+        <br />
+        <p>The queue is currently not accepting people.</p>
+        <div className={styles['refresh-button']}>
+          <Button onClick={onRefreshClick}>Refresh status</Button>
+        </div>
+      </>
+    );
   } else if (token.aheadCount === 0) {
     status = <p>There is no one ahead of you. Please wait to be notified by the queue manager.</p>;
   } else {
