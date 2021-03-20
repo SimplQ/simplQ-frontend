ce2efc0c136767989580aad932a6f2b2291a9f1c
Dalton Fury <daltonfury42@gmail.com>
2021-03-20 10:29:25 +0530

    Refresh tokens enable at client side w/o offline access

--- simplq/src/index.jsx
+++ simplq/src/index.jsx
@@ -33,7 +33,9 @@ ReactDOM.render(
     clientId="9BAywifjAy6n0sx8WbuQubMGGjofpwd6"
     redirectUri={window.location.origin}
     audience="https://devbackend.simplq.me/v1"
-    scope="read:current_user update:current_user_metadata offline_access"
+    scope="read:current_user update:current_user_metadata"
+    cacheLocation="localstorage"
+    useRefreshTokens
   >
     <ThemeProvider theme={theme}>
       <Provider store={store}>
