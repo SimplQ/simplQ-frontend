a735275b5f721a96ca1c29869b6bfa73897d368a
maaverik <nithinjosetom@gmail.com>
2021-04-12 00:27:07 +0530

    Fix return home button when reaching from errorHandler

--- simplq/src/components/ErrorHandler.jsx
+++ simplq/src/components/ErrorHandler.jsx
@@ -1,20 +1,22 @@
-/* eslint-disable */ // todo enable it back
-
 import React from 'react';
-import PageNotFound from './pages/PageNotFound';
 import { setErrorPopupMessage } from 'store/appSlice';
 import { store } from 'store';
+import * as Sentry from '@sentry/react';
+import PageNotFound from './pages/PageNotFound';
 
+// eslint-disable-next-line import/prefer-default-export
 export class ErrorBoundary extends React.Component {
   constructor(props) {
     super(props);
     this.state = { hasError: false };
   }
 
-  static getDerivedStateFromError(error) {
+  static getDerivedStateFromError() {
     // Update state so the next render will show the fallback UI.
-    console.log('Something caused a crash, falling back to 404 url, sorry...');
-    console.log(error);
+    // eslint-disable-next-line no-console
+    console.log(
+      'Something caused a crash during rendering, falling back to 404 url, please try again...'
+    );
     return { hasError: true };
   }
 
@@ -22,6 +24,11 @@ export class ErrorBoundary extends React.Component {
     // You can also log the error to an error reporting service
     // logErrorToMyService(error, errorInfo);
     store.dispatch(setErrorPopupMessage('An error occured. Please try again'));
+    // log error to sentry for alerting
+    Sentry.withScope((scope) => {
+      scope.setExtras(errorInfo);
+      Sentry.captureException(error);
+    });
   }
 
   render() {
