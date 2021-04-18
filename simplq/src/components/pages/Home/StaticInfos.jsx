5682535dbe98e6827f29861e35b552cf84ca670e
daltonfury42 <daltonfury42@users.noreply.github.com>
2021-04-18 12:48:29 +0530

    Update StaticInfos.jsx

--- simplq/src/components/pages/Home/StaticInfos.jsx
+++ simplq/src/components/pages/Home/StaticInfos.jsx
@@ -147,7 +147,9 @@ export const Pricing = () => (
         <li>
           <ul>Everything in free plan.</ul>
           <ul>20,000 tokens per month.</ul>
-          <ul>Priority Support.</ul>
+          <ul>Custom subdomains</ul>
+          <ul>Multi-user support</ul>
+          <ul>Priority Customer Support.</ul>
         </li>
         <p className={styles['amount']}>$8.99/mo</p>
         <StandardButton onClick={smoothScrollToHomePageTop}>Upgrade</StandardButton>
