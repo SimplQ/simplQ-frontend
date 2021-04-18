adf41babe3e3162abf93bb862f099ee7451659f3
daltonfury42 <daltonfury42@users.noreply.github.com>
2021-04-18 22:41:13 +0530

    Content Revamp (#614)

--- simplq/src/components/pages/Home/StaticInfos.jsx
+++ simplq/src/components/pages/Home/StaticInfos.jsx
@@ -8,19 +8,19 @@ export const BenefitsInfo = () => (
     <img src="/images/corona.png" alt="background-illustration" className={styles['corona-1']} />
     <img src="/images/corona.png" alt="background-illustration" className={styles['corona-2']} />
     <img src="/images/corona.png" alt="background-illustration" className={styles['corona-3']} />
-    <h2 data-aos="zoom-in">Why SimplQ ?</h2>
+    <h2 data-aos="zoom-in">Your customers will love it.</h2>
     <div data-aos="zoom-in" className={`${styles['container']} ${styles['benefits-container']}`}>
       <div className={styles.benefit}>
         <img src="/images/minimize_crowding.svg" alt="minimize crowding" />
-        <p>No more waiting in long lines</p>
+        <p>Reduces waiting time and improves customer satisfaction.</p>
       </div>
       <div className={styles.benefit}>
         <img src="/images/enforce_social_dist.svg" alt="Enforce social distancing" />
-        <p>Enforce social distancing effectively</p>
+        <p>Social distancing tool for post-COVID digital world.</p>
       </div>
       <div className={styles.benefit}>
         <img src="/images/check_status.svg " alt="Check your current status" />
-        <p>Check your current status in the queue</p>
+        <p>Digital tokens and live status updates.</p>
       </div>
     </div>
   </div>
@@ -33,7 +33,7 @@ export const HowToCreate = () => (
       id="target_how_it_works"
       className={`${styles['section']} ${styles['queue-info']}`}
     >
-      <h2>Create a Queue at the click of a button</h2>
+      <h2>Why you should be using SimplQ</h2>
       <div className={`${styles['container']} ${styles['queue-container']}`}>
         <div className={styles.queue}>
           <img src="/images/queue_creation.svg" alt="Queue creation" />
@@ -42,25 +42,45 @@ export const HowToCreate = () => (
           <ul className={styles['list-info']}>
             <img src="/images/wand.svg" alt="Wand" />
             <li>
-              <h3>Create Queue</h3>
+              <h3>1. Create Queue</h3>
               <p>
                 Enter queue name of your choice and create a brand new queue totally free of cost
               </p>
             </li>
             <img src="/images/share-24px.svg" alt="Share" />
             <li>
-              <h3>Share queue</h3>
-              <p>Click the share button and send the queue link to your users for them to join</p>
+              <h3>2. Share Queue</h3>
+              <p>
+                Click the share button and send the queue link to your users for them to join. Or
+                print the QR code and stick it at a place where people can scan it.
+              </p>
             </li>
             <img src="/images/people-24px.svg" alt="People" />
             <li>
-              <h3>Manage queue</h3>
+              <h3>3. Manage queue</h3>
               <p>
                 Manage your queues seamlessly and efficiently to provide a better user experience
               </p>
             </li>
           </ul>
         </div>
+        <p>
+          SimplQ customer waiting experience software is a simple and affordable ticket, table or
+          place-in-line solution, adding value to any crowded business.
+        </p>
+        <p>
+          What makes us different? Unlike other similar solutions, SimplQ is open source software
+          built by software enthusiasts from around the world. This lets us bring you the core
+          functionalities in our free plan without unnecessary fees. We also have an affordable
+          month-to-month subscription option if you want to be a long term supporter. Talk to us if
+          you need dedicated support or have any custom requirements.
+        </p>
+        <p>
+          We are generic enough to allow customers to choose the best way for them to use it, but
+          small enough to care about the individual needs of every customer.
+        </p>
+
+        <h3>This is what makes us stand out from other crowd-control solutions.</h3>
       </div>
     </div>
     <svg id={styles['curved-div-2']} xmlns="https://www.w3.org/2000/svg" viewBox="0 0 1440 50">
@@ -106,21 +126,21 @@ export const ExtraInfo = () => (
         <p className={styles['brief-description']}>Always free</p>
         <p className={styles['detailed-description']}>
           We are a team of enthusiastic developers who want to give back to society and do not
-          prioritize monetary gains
+          prioritize monetary gains.
         </p>
       </div>
       <div className={styles['card']}>
         <img src="/images/secure.svg" alt="secure" />
         <p className={styles['brief-description']}>Data security</p>
         <p className={styles['detailed-description']}>
-          Security is paramount and we take data security seriously
+          Security is paramount and we take data security seriously.
         </p>
       </div>
       <div className={styles['card']}>
         <img src="/images/easy.svg" alt="easy to use" />
         <p className={styles['brief-description']}>Easy to use</p>
         <p className={styles['detailed-description']}>
-          We provide free queue management thereby allowing you to focus on better things
+          We provide free queue management thereby allowing you to focus on better things.
         </p>
       </div>
     </div>
