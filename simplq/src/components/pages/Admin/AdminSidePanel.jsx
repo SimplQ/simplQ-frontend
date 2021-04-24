113fd4fe250035b588a21cc8f11aec5273b651cd
maaverik <nithinjosetom@gmail.com>
2021-04-24 18:41:34 +0530

    Temporarily removed components that aren't implemented

--- simplq/src/components/pages/Admin/AdminSidePanel.jsx
+++ simplq/src/components/pages/Admin/AdminSidePanel.jsx
@@ -2,17 +2,13 @@ import React from 'react';
 import SidePanel from 'components/common/SidePanel';
 import QueueDetails from './QueueInfo';
 import AddMember from './AddMember';
-import PauseQueue from './PauseQueue';
 import DeleteQueue from './DeleteQueue';
-import QueueHistory from './QueueHistory';
 import QueueSettings from './QueueSettings';
 
 export default ({ queueId }) => (
   <SidePanel>
     <AddMember queueId={queueId} />
-    <PauseQueue queueId={queueId} />
     <DeleteQueue queueId={queueId} />
-    <QueueHistory />
     <QueueDetails queueId={queueId} />
     <QueueSettings queueId={queueId} />
   </SidePanel>
--- simplq/src/components/pages/TokenStatus/TokenSidePanel.jsx
+++ simplq/src/components/pages/TokenStatus/TokenSidePanel.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import SidePanel from 'components/common/SidePanel';
-import ContactAdmin from './ContactAdmin';
 import QueueDetails from './QueueInfo';
 import NotificationContainer from './NotificationContainer';
 
 export default () => (
   <SidePanel>
-    <ContactAdmin />
     <QueueDetails />
     <NotificationContainer />
   </SidePanel>
