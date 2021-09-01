b42eef149cca7458b52a9ef890929c5d78eb91e4
anjali-001 <43542057+anjali-001@users.noreply.github.com>
2021-09-01 11:01:56 +0530

    Queue history Implemented (#646)
    * queue history implemented
    
    * queue history implemented
    
    * queue final changes
    
    * queue final changes
    
    * word wrap in container
    
    * comments resolved

--- simplq/src/components/pages/Admin/AdminSidePanel.jsx
+++ simplq/src/components/pages/Admin/AdminSidePanel.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import SidePanel from 'components/common/SidePanel';
 import QueueDetails from './QueueInfo';
 import AddMember from './AddMember';
+import QueueHistory from './QueueHistory';
 import DeleteQueue from './DeleteQueue';
 import QueueSettings from './QueueSettings';
 
@@ -9,6 +10,7 @@ export default ({ queueId }) => (
   <SidePanel>
     <AddMember queueId={queueId} />
     <DeleteQueue queueId={queueId} />
+    <QueueHistory queueId={queueId} />
     <QueueDetails queueId={queueId} />
     <QueueSettings queueId={queueId} />
   </SidePanel>
