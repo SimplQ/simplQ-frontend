4961cc04c4457efdbb6e73fbcf3a5e239e631095
Soumit Saha <soumitsaha852@gmail.com>
2021-09-05 21:25:12 +0530

    Refactored code for QrScanner.js

--- simplq/src/components/common/QrScanner/QrScanner.js
+++ simplq/src/components/common/QrScanner/QrScanner.js
@@ -1,39 +1,16 @@
 //Component responsible for the /scanQr route
 
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import QrReader from 'react-qr-scanner';
 import styles from "./QrScanner.module.scss";
 
-class QRscanner extends Component {
-  constructor(props){
-    super(props)
-    this.handleScan = this.handleScan.bind(this)
-  }
+export default () => {
 
-  componentDidMount(){
+  useEffect(() => {
     window.scrollTo('40px', '0px');
-  }
+  });
 
-// Gets the route from the url in the scan. Returns the route if the target-url 
-// is in the same domain or redirects to a target url. 
-
-  getRoute(baseurl, targeturl){
-    for(var i=0;i<baseurl.length;i++){
-      if(baseurl.charAt(i)!==targeturl.charAt(i)){
-        return {
-          verdict: false,
-          targetRoute: ""
-        }
-      }
-    }
-
-    return {
-      verdict: true,
-      targetRoute: targeturl.substring(baseurl.length)
-    }
-  }
-
-  handleScan(data){
+  const handleScan = (data) => {
     if(data!=null){
       const res = this.getRoute(window.location.origin, data.text);
 
@@ -46,32 +23,27 @@ class QRscanner extends Component {
     }
   }
 
-  handleError(err){
+  const handleError = (err) => {
     console.error(err)
   }
 
-  render(){
-    return(
-      <>
-        <div className={styles["scan-window"]}>
-          <div className={styles["description-panel"]}>
-            <p className={styles["description-para"]}>Scan a QR-Code</p>
-            <p className={styles["description-para"]}>Allow access to your camera</p>
-          </div>
-          <div className={styles["qrscan-panel"]}>
-            <QrReader 
-              delay={100} 
-              onError={this.handleError} 
-              onScan={this.handleScan}
-              className={styles["scan"]}
-              legacyMode={true}
-            />
-          </div>
+  return(
+    <>
+      <div className={styles["scan-window"]}>
+        <div className={styles["description-panel"]}>
+          <p className={styles["description-para"]}>Scan a QR-Code</p>
+          <p className={styles["description-para"]}>Allow access to your camera</p>
         </div>
-        
-      </>
-    )
-  }
+        <div className={styles["qrscan-panel"]}>
+          <QrReader 
+            delay={100} 
+            onError={handleError} 
+            onScan={handleScan}
+            className={styles["scan"]}
+            legacyMode={true}
+          />
+        </div>
+      </div>
+    </>
+  )
 }
-
-export default QRscanner;
